4da23ad25924781a2c9f2a650c6b2c7883373cad
Cesar De la Torre <cesardl@microsoft.com>
2016-10-07 14:34:20 -0700

    Updated build.sh so it also builds the eshop/ordering.api Docker Image

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 #dotnet restore
 rm -rf ./pub
-dotnet publish "$(pwd)/src/Services/Catalog/Catalog.API/project.json" -o "$(pwd)/pub/catalog"
 dotnet publish "$(pwd)/src/Web/Microsoft.eShopOnContainers.WebMVC/project.json" -o "$(pwd)/pub/webMVC"
+dotnet publish "$(pwd)/src/Services/Catalog/Catalog.API/project.json" -o "$(pwd)/pub/catalog"
+dotnet publish "$(pwd)/src/Services/Ordering/Ordering.API/project.json" -o "$(pwd)/pub/ordering"
 
 docker build -t eshop/web "$(pwd)/pub/webMVC"
 docker build -t eshop/catalog.api "$(pwd)/pub/catalog"
+docker build -t eshop/ordering.api "$(pwd)/pub/ordering"
