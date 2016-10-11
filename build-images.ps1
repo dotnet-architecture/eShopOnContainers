2e43db49539901c8b224c7e7232c553ad97482ba
Cesar De la Torre <cesardl@microsoft.com>
2016-10-10 19:38:26 -0700

    Extended build-images with dotnet restore commands

--- build-images.ps1
+++ build-images.ps1
@@ -7,23 +7,34 @@ $pubFolderToDelete = $scriptPath + "\pub"
 remove-item -path $pubFolderToDelete -Force -Recurse -ErrorAction SilentlyContinue
 #cmd /c "rd /s pub" /q
 
-
+# *** WebMVC image ***
 $webPathToJson = $scriptPath + "\src\Web\WebMVC\project.json"
 Write-Host "webPathToJson is $webPathToJson" -ForegroundColor Yellow
 $webPathToPub = $scriptPath + "\pub\webMVC"
 Write-Host "webPathToPub is $webPathToPub" -ForegroundColor Yellow
+
+Write-Host "Restore Dependencies just in case as it is needed to run dotnet publish" -ForegroundColor Blue
+dotnet restore $webPathToJson
 dotnet publish $webPathToJson -o $webPathToPub
 
+#*** Catalog service image ***
 $catalogPathToJson = $scriptPath + "\src\Services\Catalog\Catalog.API\project.json"
 Write-Host "catalogPathToJson is $catalogPathToJson" -ForegroundColor Yellow
 $catalogPathToPub = $scriptPath + "\pub\catalog"
 Write-Host "catalogPathToPub is $catalogPathToPub" -ForegroundColor Yellow
+
+Write-Host "Restore Dependencies just in case as it is needed to run dotnet publish" -ForegroundColor Blue
+dotnet restore $catalogPathToJson
 dotnet publish $catalogPathToJson -o $catalogPathToPub
 
+#*** Ordering service image ***
 $orderingPathToJson = $scriptPath + "\src\Services\Ordering\Ordering.API\project.json"
 Write-Host "orderingPathToJson is $orderingPathToJson" -ForegroundColor Yellow
 $orderingPathToPub = $scriptPath + "\pub\ordering"
 Write-Host "orderingPathToPub is $orderingPathToPub" -ForegroundColor Yellow
+
+Write-Host "Restore Dependencies just in case as it is needed to run dotnet publish" -ForegroundColor Blue
+dotnet restore $orderingPathToJson
 dotnet publish $orderingPathToJson -o $orderingPathToPub
  
 
