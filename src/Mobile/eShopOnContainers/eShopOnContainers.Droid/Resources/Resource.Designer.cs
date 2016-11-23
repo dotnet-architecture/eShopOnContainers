e02b372ac2e72577d75237887c67dfdfbe1589ae
Javier Suárez Ruiz <jsuarez@plainconcepts.com>
2016-11-23 08:21:52 +0100

    Added NoImage to all mobile platforms

--- src/Mobile/eShopOnContainers/eShopOnContainers.Droid/Resources/Resource.Designer.cs
+++ src/Mobile/eShopOnContainers/eShopOnContainers.Droid/Resources/Resource.Designer.cs
@@ -2314,26 +2314,29 @@ namespace eShopOnContainers.Droid
 			// aapt resource value: 0x7f0200ac
 			public const int mr_ic_play_light = 2130837676;
 			
-			// aapt resource value: 0x7f0200b3
-			public const int notification_template_icon_bg = 2130837683;
-			
 			// aapt resource value: 0x7f0200ad
-			public const int product_add = 2130837677;
+			public const int noimage = 2130837677;
+			
+			// aapt resource value: 0x7f0200b4
+			public const int notification_template_icon_bg = 2130837684;
 			
 			// aapt resource value: 0x7f0200ae
-			public const int roundedbg = 2130837678;
+			public const int product_add = 2130837678;
 			
 			// aapt resource value: 0x7f0200af
-			public const int roundedbgdark = 2130837679;
+			public const int roundedbg = 2130837679;
 			
 			// aapt resource value: 0x7f0200b0
-			public const int splash_drawable = 2130837680;
+			public const int roundedbgdark = 2130837680;
 			
 			// aapt resource value: 0x7f0200b1
-			public const int switch_off = 2130837681;
+			public const int splash_drawable = 2130837681;
 			
 			// aapt resource value: 0x7f0200b2
-			public const int switch_on = 2130837682;
+			public const int switch_off = 2130837682;
+			
+			// aapt resource value: 0x7f0200b3
+			public const int switch_on = 2130837683;
 			
 			static Drawable()
 			{
