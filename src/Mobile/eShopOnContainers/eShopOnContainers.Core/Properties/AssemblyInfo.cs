359ed56b75a1e63a05a84a6d5a56721b6a73ea7e
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2016-12-20 10:47:45 +0100

    Fixed Xamarin project package restore error

--- src/Mobile/eShopOnContainers/eShopOnContainers.Core/Properties/AssemblyInfo.cs
+++ src/Mobile/eShopOnContainers/eShopOnContainers.Core/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("eShopOnContainers")]
+[assembly: AssemblyTitle("eShopOnContainers.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("eShopOnContainers")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("eShopOnContainers.Core")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
