9525024649e1489768f873aaff3f3a467ad533e0
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2017-01-11 15:48:16 +0100

    Changed Logout callback URL

--- src/Mobile/eShopOnContainers/eShopOnContainers.Core/GlobalSettings.cs
+++ src/Mobile/eShopOnContainers/eShopOnContainers.Core/GlobalSettings.cs
@@ -61,7 +61,7 @@
             UserInfoEndpoint = string.Format("{0}:5105/connect/userinfo", baseEndpoint);
             LogoutEndpoint = string.Format("{0}:5105/connect/endsession", baseEndpoint);
             IdentityCallback = "http://eshopxamarin/callback.html";
-            LogoutCallback = "http://eshopxamarin/callback.html/index.html";
+            LogoutCallback = string.Format("{0}:5105/Account/InProgress", baseEndpoint);
         }
     }
 }
