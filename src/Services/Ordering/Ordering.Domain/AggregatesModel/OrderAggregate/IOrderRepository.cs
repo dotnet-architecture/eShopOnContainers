795c786625f3a15c9b49661cd1ffd13bb86d6fa3
CESARDL <CESARDL@SURFBOOKCESARDL>
2017-02-02 16:10:06 -0800

    Renamed IRepository to IAggregateRepository

--- src/Services/Ordering/Ordering.Domain/AggregatesModel/OrderAggregate/IOrderRepository.cs
+++ src/Services/Ordering/Ordering.Domain/AggregatesModel/OrderAggregate/IOrderRepository.cs
@@ -5,7 +5,7 @@ namespace Microsoft.eShopOnContainers.Services.Ordering.Domain.AggregatesModel.O
     //This is just the RepositoryContracts or Interface defined at the Domain Layer
     //as requisite for the Order Aggregate
     public interface IOrderRepository
-        :IRepository
+        :IAggregateRepository
     {
         Order Add(Order order);
     }
