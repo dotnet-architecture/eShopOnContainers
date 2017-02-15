ffeabf8f655dbf548e0f2e9ee5e9f8272f7d1d8a
Bill Wagner <wiwagn@microsoft.com>
2017-02-15 10:44:59 -0500

    update build-images for Mac
    Until adding these, the project would not build correctly on a Mac.

--- build-images.sh
+++ build-images.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
-#dotnet restore
+dotnet restore
 rm -rf ./pub
 dotnet publish "$(pwd)/src/Web/WebMVC/project.json" -o "$(pwd)/pub/webMVC"
 dotnet publish "$(pwd)/src/Services/Catalog/Catalog.API/project.json" -o "$(pwd)/pub/catalog"
 dotnet publish "$(pwd)/src/Services/Ordering/Ordering.API/project.json" -o "$(pwd)/pub/ordering"
 dotnet publish "$(pwd)/src/Services/Basket/Basket.API/project.json" -o "$(pwd)/pub/basket"
+dotnet publish "$(pwd)/src/Services/Identity/Identity.API/project.json" -o "$(pwd)/pub/identity"
+dotnet publish "$(pwd)/src/Web/WebSPA/eShopOnContainers.WebSPA/project.json" -o "$(pwd)/pub/WebSPA"
 
 docker build -t eshop/web "$(pwd)/pub/webMVC"
 docker build -t eshop/catalog.api "$(pwd)/pub/catalog"
 docker build -t eshop/ordering.api "$(pwd)/pub/ordering"
 docker build -t eshop/basket.api "$(pwd)/pub/basket"
+docker build -t eshop/identity "$(pwd)/pub/identity"
+docker build -t eshop/webspa "$(pwd)/pub/WebSPA"
