4e2583477b51bdd4f0808535ad9f87d6422fe7ed
Bill Wagner <wiwagn@microsoft.com>
2017-02-15 23:10:56 -0500

    script working for SPA

--- build-images.sh
+++ build-images.sh
@@ -5,12 +5,19 @@ dotnet publish "$(pwd)/src/Web/WebMVC/project.json" -o "$(pwd)/pub/webMVC"
 dotnet publish "$(pwd)/src/Services/Catalog/Catalog.API/project.json" -o "$(pwd)/pub/catalog"
 dotnet publish "$(pwd)/src/Services/Ordering/Ordering.API/project.json" -o "$(pwd)/pub/ordering"
 dotnet publish "$(pwd)/src/Services/Basket/Basket.API/project.json" -o "$(pwd)/pub/basket"
+
+pushd "$(pwd)/src/Web/WebSPA/eShopOnContainers.WebSPA"
+npm install
+npm run build:prod
+popd
+
+dotnet publish "$(pwd)/src/Web/WebSPA/eShopOnContainers.WebSPA/project.json" -o "$(pwd)/pub/webSPA"
+
 dotnet publish "$(pwd)/src/Services/Identity/Identity.API/project.json" -o "$(pwd)/pub/identity"
-dotnet publish "$(pwd)/src/Web/WebSPA/eShopOnContainers.WebSPA/project.json" -o "$(pwd)/pub/WebSPA"
 
 docker build -t eshop/web "$(pwd)/pub/webMVC"
 docker build -t eshop/catalog.api "$(pwd)/pub/catalog"
 docker build -t eshop/ordering.api "$(pwd)/pub/ordering"
 docker build -t eshop/basket.api "$(pwd)/pub/basket"
+docker build -t eshop/webspa "$(pwd)/pub/webSPA"
 docker build -t eshop/identity "$(pwd)/pub/identity"
-docker build -t eshop/webspa "$(pwd)/pub/WebSPA"
