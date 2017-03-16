4eed41e2aae5d03afc8c8fcb0db8c6b141c458fa
Bill Wagner <wiwagn@microsoft.com>
2017-03-15 21:39:41 -0400

    Finish all list page style updates.

--- src/Web/Catalog.WebForms/Catalog.WebForms/Default.aspx.designer.cs
+++ src/Web/Catalog.WebForms/Catalog.WebForms/Default.aspx.designer.cs
@@ -13,15 +13,6 @@ namespace Microsoft.eShopOnContainers.Catalog.WebForms {
     public partial class _Default {
         
         /// <summary>
-        /// catalogList control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ListView catalogList;
-        
-        /// <summary>
         /// DataPager1 control.
         /// </summary>
         /// <remarks>
@@ -29,5 +20,14 @@ namespace Microsoft.eShopOnContainers.Catalog.WebForms {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.DataPager DataPager1;
+        
+        /// <summary>
+        /// catalogList control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListView catalogList;
     }
 }
