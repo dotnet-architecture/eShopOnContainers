bb39e5fd6eda370937f7c7ce05e94e544ab0dcfb
Cesar De la Torre <cesardl@microsoft.com>
2017-03-21 12:58:07 -0700

    Minor comment change

--- src/Services/Ordering/Ordering.Infrastructure/OrderingContext.cs
+++ src/Services/Ordering/Ordering.Infrastructure/OrderingContext.cs
@@ -241,8 +241,8 @@ namespace Microsoft.eShopOnContainers.Services.Ordering.Infrastructure
             // Choices:
             // A) Right BEFORE committing data (EF SaveChanges) into the DB will make a single transaction including  
             // side effects from the domain event handlers which are using the same DbContext with "InstancePerLifetimeScope" or "scoped" lifetime
-            // B) Right AFTER committing data (EF SaveChanges) into the DB. will make multiple transactions. 
-            // You will need to handle eventual consistency and compensatory actions in case of failures. 
+            // B) Right AFTER committing data (EF SaveChanges) into the DB will make multiple transactions. 
+            // You will need to handle eventual consistency and compensatory actions in case of failures in any of the Handlers. 
             await _mediator.DispatchDomainEventsAsync(this);
 
 
