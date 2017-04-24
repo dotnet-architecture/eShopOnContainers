40dddbaa3f94771ddd427a1cdd23eeba85ae23bb
David Britch <davidbritch@Davids-MBP.lan>
2017-04-24 17:37:53 +0100

    View model unit tests added.

---
+++ src/Mobile/eShopOnContainers/eShopOnContainers.UnitTests/ViewModels/MainViewModelTests.cs
@@ -0,0 +1,56 @@
+﻿using Xunit;
+using eShopOnContainers.Core.ViewModels;
+using eShopOnContainers.Core.ViewModels.Base;
+using eShopOnContainers.Core.Models.Navigation;
+using System.Threading.Tasks;
+
+namespace eShopOnContainers.UnitTests
+{
+	public class MainViewModelTests
+	{
+		[Fact]
+		public void SettingsCommandIsNotNullWhenViewModelInstantiatedTest()
+		{
+			ViewModelLocator.RegisterDependencies(true);
+			var mainViewModel = new MainViewModel();
+
+			Assert.NotNull(mainViewModel.SettingsCommand);
+		}
+
+		[Fact]
+		public async Task ViewModelInitializationSendsChangeTabMessageTest()
+		{
+			bool messageReceived = false;
+			ViewModelLocator.RegisterDependencies(true);
+			var mainViewModel = new MainViewModel();
+			var tabParam = new TabParameter { TabIndex = 2 };
+
+			Xamarin.Forms.MessagingCenter.Subscribe<MainViewModel, int>(this, MessageKeys.ChangeTab, (sender, arg) =>
+			{
+				messageReceived = true;
+			});
+			await mainViewModel.InitializeAsync(tabParam);
+
+			Assert.True(messageReceived);
+		}
+
+		[Fact]
+		public void IsBusyPropertyIsFalseWhenViewModelInstantiatedTest()
+		{
+			ViewModelLocator.RegisterDependencies(true);
+			var mainViewModel = new MainViewModel();
+			Assert.False(mainViewModel.IsBusy);
+		}
+
+		[Fact]
+		public async Task IsBusyPropertyIsTrueAfterViewModelInitializationTest()
+		{
+			ViewModelLocator.RegisterDependencies(true);
+			var mainViewModel = new MainViewModel();
+
+			await mainViewModel.InitializeAsync(null);
+
+			Assert.True(mainViewModel.IsBusy);
+		}
+	}
+}
---
+++ src/Mobile/eShopOnContainers/eShopOnContainers.UnitTests/ViewModels/OrderViewModelTests.cs
@@ -0,0 +1,55 @@
+﻿using Xunit;
+using eShopOnContainers.Core;
+using eShopOnContainers.Core.ViewModels;
+using eShopOnContainers.Core.ViewModels.Base;
+using eShopOnContainers.Core.Services.Order;
+using System.Threading.Tasks;
+
+namespace eShopOnContainers.UnitTests
+{
+	public class OrderViewModelTests
+	{
+		[Fact]
+		public void OrderPropertyIsNullWhenViewModelInstantiatedTest()
+		{
+			var orderService = new OrderMockService();
+			ViewModelLocator.RegisterDependencies(true);
+			var orderViewModel = new OrderDetailViewModel(orderService);
+
+			Assert.Null(orderViewModel.Order);
+		}
+
+		[Fact]
+		public async Task OrderPropertyIsNotNullAfterViewModelInitializationTest()
+		{
+			var orderService = new OrderMockService();
+			ViewModelLocator.RegisterDependencies(true);
+			var orderViewModel = new OrderDetailViewModel(orderService);
+
+			var order = await orderService.GetOrderAsync(1, GlobalSetting.Instance.AuthToken);
+			await orderViewModel.InitializeAsync(order);
+
+			Assert.NotNull(orderViewModel.Order);
+		}
+
+		[Fact]
+		public async Task SettingOrderPropertyShouldRaisePropertyChanged()
+		{
+			bool invoked = false;
+
+			var orderService = new OrderMockService();
+			ViewModelLocator.RegisterDependencies(true);
+			var orderViewModel = new OrderDetailViewModel(orderService);
+
+			orderViewModel.PropertyChanged += (sender, e) =>
+			{
+				if (e.PropertyName.Equals("Order"))
+					invoked = true;
+			};
+			var order = await orderService.GetOrderAsync(1, GlobalSetting.Instance.AuthToken);
+			await orderViewModel.InitializeAsync(order);
+
+			Assert.True(invoked);
+		}
+	}
+}
