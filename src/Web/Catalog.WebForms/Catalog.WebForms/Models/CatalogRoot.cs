9c2318726b82e6bc927305c5520cc6b439cc7b94
Bill Wagner <wiwagn@microsoft.com>
2017-05-03 15:13:10 -0400

    interim checkin
    UPdated all projects.
    The app (mostly) works, but the RabbitMQ container is failing.

---
+++ src/Web/Catalog.WebForms/Catalog.WebForms/Models/CatalogRoot.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace eShopOnContainers.Core.Models.Catalog
+{
+    public class CatalogRoot
+    {
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public int Count { get; set; }
+        public List<CatalogItem> Data { get; set; }
+    }
+}
---
+++ src/Web/Catalog.WebForms/Catalog.WebForms/Services/ICatalogService.cs
@@ -0,0 +1,21 @@
+﻿using eShopOnContainers.Core.Models.Catalog;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+// Taken from https://github.com/dotnet/eShopOnContainers/blob/vs2017/src/Mobile/eShopOnContainers/eShopOnContainers.Core/Services/Catalog/ICatalogService.cs
+// Open Issue: How do we make this application DRY and still support the story
+// of a 'monolithic' app for a 'Lift and Shift' scenario?
+namespace eShopOnContainers.Core.Services.Catalog
+{
+    public interface ICatalogService
+    {
+        Task<ObservableCollection<CatalogBrand>> GetCatalogBrandAsync();
+        Task<ObservableCollection<CatalogItem>> FilterAsync(int catalogBrandId, int catalogTypeId);
+        Task<ObservableCollection<CatalogType>> GetCatalogTypeAsync();
+        Task<ObservableCollection<CatalogItem>> GetCatalogAsync();
+        Task<CatalogItem> GetCatalogItemAsync(string id);
+        Task DeleteCatalogItemAsync(string catalogItemId);
+        Task<CatalogItem> UpdateCatalogItemAsync(CatalogItem item);
+        Task<CatalogItem> CreateCatalogItemAsync(CatalogItem item);
+    }
+}
