90482cc32a004b41d09d666c6c46d5fe47435de8
David Britch <davidbritch@Davids-MBP.lan>
2017-05-05 15:18:26 +0100

    Fixed Windows unit test runner project.

--- src/Mobile/eShopOnContainers/eShopOnContainers.TestRunner.Windows/Properties/AssemblyInfo.cs
+++ src/Mobile/eShopOnContainers/eShopOnContainers.TestRunner.Windows/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("eShopOnContainers.TestRunner.Windows")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
