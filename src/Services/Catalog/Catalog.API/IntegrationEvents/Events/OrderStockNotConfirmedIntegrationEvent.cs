fbfe287da5c4756111ceb65d9eb784cd2f27a1e7
Christian Arenas <carenas@plainconcepts.com>
2017-05-09 13:59:34 +0200

    Add ConfirmOrderStockIntegrationEventHandler and OrderStockConfirmedIntegrationEvent/OrderStockNotConfirmedIntegrationEvent

---
+++ src/Services/Catalog/Catalog.API/IntegrationEvents/Events/OrderStockNotConfirmedIntegrationEvent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Microsoft.eShopOnContainers.Services.Catalog.API.IntegrationEvents.Events
+{
+    using BuildingBlocks.EventBus.Events;
+
+    public class OrderStockNotConfirmedIntegrationEvent : IntegrationEvent
+    {
+        public int OrderId { get; }
+
+        public IEnumerable<ConfirmedOrderStockItem> OrderStockItem { get; }
+
+        public OrderStockNotConfirmedIntegrationEvent(int orderId, 
+            IEnumerable<ConfirmedOrderStockItem> orderStockItem)
+        {
+            OrderId = orderId;
+            OrderStockItem = orderStockItem;
+        }
+
+        public class ConfirmedOrderStockItem
+        {
+            public int ProductId { get; }
+            public int Units { get; }
+            public bool Confirmed { get; }
+
+            public ConfirmedOrderStockItem(int productId, int units, bool confirmed)
+            {
+                ProductId = productId;
+                Units = units;
+                Confirmed = confirmed;
+            }
+        }
+    }
+}
