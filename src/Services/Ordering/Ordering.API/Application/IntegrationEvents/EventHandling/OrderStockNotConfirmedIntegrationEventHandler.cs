870ae0dabd72e9a64166e38fbfe9f1017834d492
Christian Arenas <carenas@plainconcepts.com>
2017-05-09 13:58:48 +0200

    Add Confirm Grade period Handler and add new items to Orders status

---
+++ src/Services/Ordering/Ordering.API/Application/IntegrationEvents/EventHandling/OrderStockNotConfirmedIntegrationEventHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace Ordering.API.Application.IntegrationEvents.EventHandling
+{
+    using Microsoft.eShopOnContainers.BuildingBlocks.EventBus.Abstractions;
+    using System;
+    using System.Threading.Tasks;
+    using Events;
+
+    public class OrderStockNotConfirmedIntegrationEventHandler : IIntegrationEventHandler<OrderStockNotConfirmedIntegrationEvent>
+    {
+        public async Task Handle(OrderStockNotConfirmedIntegrationEvent @event)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
