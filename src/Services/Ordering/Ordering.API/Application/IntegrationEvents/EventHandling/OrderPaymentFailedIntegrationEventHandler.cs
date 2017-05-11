bf6360a82ad8be2f4216f3a55aadf1a6dfb11c9e
Christian Arenas <carenas@plainconcepts.com>
2017-05-11 16:05:27 +0200

    Add Order Payment Integration events

---
+++ src/Services/Ordering/Ordering.API/Application/IntegrationEvents/EventHandling/OrderPaymentFailedIntegrationEventHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace Ordering.API.Application.IntegrationEvents.EventHandling
+{
+    using Microsoft.eShopOnContainers.BuildingBlocks.EventBus.Abstractions;
+    using Ordering.API.Application.IntegrationEvents.Events;
+    using System.Threading.Tasks;
+
+    public class OrderPaymentFailedIntegrationEventHandler : 
+        IIntegrationEventHandler<OrderPaymentFailedIntegrationEvent>
+    {
+        public async Task Handle(OrderPaymentFailedIntegrationEvent @event)
+        {
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.API/Application/IntegrationEvents/Events/OrderPaymentFailedIntegrationEvent .cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.eShopOnContainers.BuildingBlocks.EventBus.Events;
+
+namespace Ordering.API.Application.IntegrationEvents.Events
+{
+    public class OrderPaymentFailedIntegrationEvent : IntegrationEvent
+    {
+        public int OrderId { get; }
+
+        public OrderPaymentFailedIntegrationEvent(int orderId) => OrderId = orderId;
+    }
+}
