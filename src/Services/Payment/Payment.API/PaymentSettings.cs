a84c180e07386145bddf744ae23b0004f7b5b689
Christian Arenas <carenas@plainconcepts.com>
2017-05-17 19:42:45 +0200

    Remove PaymentIntegrationEventService and remove integration command to integration event

---
+++ src/Services/Payment/Payment.API/PaymentSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Payment.API
+{
+    public class PaymentSettings
+    {
+        public bool SuccessPayment { get; set; }
+        public string EventBusConnection { get; set; }
+    }
+}
