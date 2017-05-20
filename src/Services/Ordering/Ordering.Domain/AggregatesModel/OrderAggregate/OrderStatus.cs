ce2083d2485fcda885285f4a1ea039e4b2b1abbb
Cesar De la Torre <cesardl@microsoft.com>
2017-05-20 12:35:16 -0700

    Minor refactoring

--- src/Services/Ordering/Ordering.Domain/AggregatesModel/OrderAggregate/OrderStatus.cs
+++ src/Services/Ordering/Ordering.Domain/AggregatesModel/OrderAggregate/OrderStatus.cs
@@ -10,7 +10,7 @@
     public class OrderStatus
         : Enumeration
     {
-        public static OrderStatus Submited = new OrderStatus(1, nameof(Submited).ToLowerInvariant());
+        public static OrderStatus Submitted = new OrderStatus(1, nameof(Submitted).ToLowerInvariant());
         public static OrderStatus AwaitingValidation = new OrderStatus(2, nameof(AwaitingValidation).ToLowerInvariant());
         public static OrderStatus StockConfirmed = new OrderStatus(3, nameof(StockConfirmed).ToLowerInvariant());
         public static OrderStatus Paid = new OrderStatus(4, nameof(Paid).ToLowerInvariant());
@@ -27,7 +27,7 @@
         }
 
         public static IEnumerable<OrderStatus> List() =>
-            new[] { Submited, AwaitingValidation, StockConfirmed, Paid, Shipped, Cancelled };
+            new[] { Submitted, AwaitingValidation, StockConfirmed, Paid, Shipped, Cancelled };
 
         public static OrderStatus FromName(string name)
         {
