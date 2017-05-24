1218069eaf87a6e26827b7e27fe548f68507f519
Ramón Tomás <ramon.tomas84@hotmail.com>
2017-05-24 08:53:36 +0200

    Removed Saga feature

---
+++ src/Services/Ordering/Ordering.API/Application/IntegrationEvents/EventHandling/GracePeriodConfirmedIntegrationEventHandler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.eShopOnContainers.BuildingBlocks.EventBus.Abstractions;
+using Microsoft.eShopOnContainers.Services.Ordering.Domain.AggregatesModel.OrderAggregate;
+using Ordering.API.Application.IntegrationEvents.Events;
+using System.Threading.Tasks;
+
+namespace Ordering.API.Application.IntegrationEvents.EventHandling
+{
+    public class GracePeriodConfirmedIntegrationEventHandler : IIntegrationEventHandler<GracePeriodConfirmedIntegrationEvent>
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public GracePeriodConfirmedIntegrationEventHandler(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        /// <summary>
+        /// Event handler which confirms that the grace period
+        /// has been completed and order will not initially be cancelled.
+        /// Therefore, the order process continues for validation. 
+        /// </summary>
+        /// <param name="event">       
+        /// </param>
+        /// <returns></returns>
+        public async Task Handle(GracePeriodConfirmedIntegrationEvent @event)
+        {
+            var orderToUpdate = await _orderRepository.GetAsync(@event.OrderId);
+            orderToUpdate.SetAwaitingValidationStatus();
+            await _orderRepository.UnitOfWork.SaveEntitiesAsync();
+        }
+    }
+}
