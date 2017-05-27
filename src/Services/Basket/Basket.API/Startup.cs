c23d2551cde82bb0fdc7e8e74cedcd352c01e1cc
Christian Arenas <christian147_@hotmail.com>
2017-05-27 18:35:28 +0200

    Add new redis connection getting data from environment variable

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -16,11 +16,8 @@ using Microsoft.Extensions.HealthChecks;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using RabbitMQ.Client;
-using StackExchange.Redis;
-using System.Linq;
-using System.Net;
 using System.Threading.Tasks;
-using System;
+using StackExchange.Redis;
 
 namespace Microsoft.eShopOnContainers.Services.Basket.API
 {
@@ -64,11 +61,11 @@ namespace Microsoft.eShopOnContainers.Services.Basket.API
             services.AddSingleton<ConnectionMultiplexer>(sp =>
             {
                 var settings = sp.GetRequiredService<IOptions<BasketSettings>>().Value;
-                var ips = Dns.GetHostAddressesAsync(settings.ConnectionString).Result;
+                var configuration = ConfigurationOptions.Parse(settings.ConnectionString, true);
+                configuration.ResolveDns = true;
 
-                return ConnectionMultiplexer.Connect(ips.First().ToString());
+                return ConnectionMultiplexer.Connect(configuration);
             });
-
 
             services.AddSingleton<IRabbitMQPersistentConnection>(sp =>
             {
