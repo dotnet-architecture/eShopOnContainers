baab0d2a98a8ec4b539a3e1f6a476649328a70a7
Christian Arenas <carenas@plainconcepts.com>
2017-06-01 20:26:10 +0200

    Remove unused class

--- src/Services/Marketing/Marketing.API/Model/Location.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-namespace Microsoft.eShopOnContainers.Services.Marketing.API.Model
-{
-    public class Location
-    {
-        public int Id { get; set; }
-    }
-}
