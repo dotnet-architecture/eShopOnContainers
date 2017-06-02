78421946374a8d36676942828288239f3252beb2
Christian Arenas <christian.arenasnavarro@hotmail.com>
2017-06-03 01:00:55 +0200

    Create RuleType class to manage RuleTypeEnum

---
+++ src/Services/Marketing/Marketing.API/Dto/RuleTypeEnum.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.eShopOnContainers.Services.Marketing.API.Infrastructure.Exceptions;
+using System;
+
+namespace Microsoft.eShopOnContainers.Services.Marketing.API.Dto
+{
+    public enum RuleTypeEnum { UserProfileRule = 1, PurchaseHistoryRule = 2, UserLocationRule = 3 }
+
+    public static class RuleType
+    {
+        public static RuleTypeEnum From(int id)
+        {
+            if (!Enum.IsDefined(typeof(RuleTypeEnum), id))
+            {
+                throw new MarketingDomainException($"Invalid value for RuleType, RuleTypeId: {id}");
+            }
+
+            return (RuleTypeEnum)id;
+        }
+    }
+}
