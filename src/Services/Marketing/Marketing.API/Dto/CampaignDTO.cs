d4ab5b24d2ad45f86c232c8bbca42cfa4e8db0d0
Christian Arenas <carenas@plainconcepts.com>
2017-06-02 16:30:39 +0200

    Add dtos

---
+++ src/Services/Marketing/Marketing.API/Dto/CampaignDTO.cs
@@ -0,0 +1,25 @@
+﻿namespace Microsoft.eShopOnContainers.Services.Marketing.API.Dto
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class CampaignDTO
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime From { get; set; }
+
+        public DateTime To { get; set; }
+
+        public string Url { get; set; }
+
+        public List<RuleDTO> Rules { get; set; }
+
+        public CampaignDTO()
+        {
+            Rules = new List<RuleDTO>();
+        }
+    }
+}
---
+++ src/Services/Marketing/Marketing.API/Dto/RuleDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace Microsoft.eShopOnContainers.Services.Marketing.API.Dto
+{
+    public class RuleDTO
+    {
+        public int Id { get; set; }
+
+        public RuleTypeEnum RuleType => (RuleTypeEnum) RuleTypeId;
+
+        public int RuleTypeId { get; set; }
+
+        public int CampaignId { get; set; }
+
+        public int? LocationId { get; set; }
+
+        public string Description { get; set; }
+    }
+
+    public enum RuleTypeEnum { UserProfileRule = 1, PurchaseHistoryRule = 2, UserLocationRule = 3 }
+}
