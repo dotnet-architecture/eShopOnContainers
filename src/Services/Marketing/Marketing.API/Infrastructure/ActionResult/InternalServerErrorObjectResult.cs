1bd13747f47d7ed375b636fbe01d14f4b9f6775a
Christian Arenas <carenas@plainconcepts.com>
2017-06-02 16:31:03 +0200

    Add exceptions filters

---
+++ src/Services/Marketing/Marketing.API/Infrastructure/ActionResult/InternalServerErrorObjectResult.cs
@@ -0,0 +1,14 @@
+﻿namespace Microsoft.eShopOnContainers.Services.Marketing.API.Infrastructure.ActionResults
+{
+    using AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class InternalServerErrorObjectResult : ObjectResult
+    {
+        public InternalServerErrorObjectResult(object error)
+            : base(error)
+        {
+            StatusCode = StatusCodes.Status500InternalServerError;
+        }
+    }
+}
