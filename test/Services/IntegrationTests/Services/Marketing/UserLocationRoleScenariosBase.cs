6ff6864f912be6a4434d3e8c9af57ecc7193b3ad
Christian Arenas <carenas@plainconcepts.com>
2017-06-06 14:19:01 +0200

    Edit the integration test

---
+++ test/Services/IntegrationTests/Services/Marketing/UserLocationRoleScenariosBase.cs
@@ -0,0 +1,40 @@
+﻿namespace IntegrationTests.Services.Marketing
+{
+    public class UserLocationRoleScenariosBase : MarketingScenarioBase
+    {
+        private const string EndpointLocationName = "locations";
+        public static class Get
+        {
+            public static string UserLocationRulesByCampaignId(int campaignId)
+                => GetUserLocationRolesUrlBase(campaignId);
+
+            public static string UserLocationRuleByCampaignAndUserLocationRuleId(int campaignId,
+                int userLocationRuleId)
+                => $"{GetUserLocationRolesUrlBase(campaignId)}/{userLocationRuleId}";
+        }
+
+        public static class Post
+        {
+            public static string AddNewuserLocationRule(int campaignId) 
+                => GetUserLocationRolesUrlBase(campaignId);
+        }
+
+        public static class Put
+        {
+            public static string UserLocationRoleBy(int campaignId,
+                int userLocationRuleId)
+                => $"{GetUserLocationRolesUrlBase(campaignId)}/{userLocationRuleId}";
+        }
+
+        public static class Delete
+        {
+            public static string UserLocationRoleBy(int campaignId,
+                int userLocationRuleId)
+                => $"{GetUserLocationRolesUrlBase(campaignId)}/{userLocationRuleId}";
+        }
+
+
+        private static string GetUserLocationRolesUrlBase(int campaignId)
+            => $"{CampaignsUrlBase}/{campaignId}/{EndpointLocationName}";
+    }
+}
