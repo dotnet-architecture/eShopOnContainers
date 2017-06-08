05bd964f2b8279cc6322458ed40d7f17b9067edc
Ramón Tomás <ramon.tomas84@hotmail.com>
2017-06-08 17:33:55 +0200

    Adapt Locations.api to nosql db

--- src/Services/Location/Locations.API/Infrastructure/Services/ILocationsService.cs
+++ src/Services/Location/Locations.API/Infrastructure/Services/ILocationsService.cs
@@ -1,10 +1,16 @@
 ﻿namespace Microsoft.eShopOnContainers.Services.Locations.API.Infrastructure.Services
 {
+    using Microsoft.eShopOnContainers.Services.Locations.API.Model;
     using Microsoft.eShopOnContainers.Services.Locations.API.ViewModel;
+    using System.Collections.Generic;
     using System.Threading.Tasks;
 
     public interface ILocationsService
     {
+        Task<UserLocation> GetUserLocation(int id);
+
+        Task<List<Locations>> GetAllLocation();
+
         Task<bool> AddOrUpdateUserLocation(string userId, LocationRequest locRequest);
     }
 }
