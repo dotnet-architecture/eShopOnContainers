cdd15117fbbe35d9feb1a6900b80d1d9b13ffe32
Christian Arenas <carenas@plainconcepts.com>
2017-06-09 11:59:56 +0200

    Add locationService class

---
+++ src/Mobile/eShopOnContainers/eShopOnContainers.Core/Models/Location/LocationRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace eShopOnContainers.Core.Models.Location
+{
+    public class LocationRequest
+    {
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+    }
+}
