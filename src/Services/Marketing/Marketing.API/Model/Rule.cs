1d80de4b09243a8225892724a1433c38d8f9fb22
Christian Arenas <carenas@plainconcepts.com>
2017-06-09 12:50:26 +0200

    add new migration

--- src/Services/Marketing/Marketing.API/Model/Rule.cs
+++ src/Services/Marketing/Marketing.API/Model/Rule.cs
@@ -9,25 +9,19 @@
         public Campaign Campaign { get; set; }
         
         public string Description { get; set; }
-
-        public abstract int RuleTypeId { get;}
     }
 
 
     public class UserProfileRule : Rule
     {
-        public override int RuleTypeId => (int)RuleTypeEnum.UserProfileRule;
     }
 
     public class PurchaseHistoryRule : Rule
     {
-        public override int RuleTypeId => (int)RuleTypeEnum.PurchaseHistoryRule;
     }
 
     public class UserLocationRule : Rule
     {
-        public override int RuleTypeId => (int)RuleTypeEnum.UserLocationRule;
-
         public int LocationId { get; set; }
     }
 }
