73ac99bc70a1a9e0a25299ed5e0314fdce7e12a7
Christian Arenas <carenas@plainconcepts.com>
2017-06-09 11:59:56 +0200

    Add locationService class

---
+++ src/Mobile/eShopOnContainers/eShopOnContainers.Core/Services/Location/ILocationService.cs
@@ -0,0 +1,10 @@
+﻿namespace eShopOnContainers.Core.Services.Location
+{
+    using System.Threading.Tasks;
+    using eShopOnContainers.Core.Models.Location;
+    
+    public interface ILocationService
+    {
+        Task UpdateUserLocation(LocationRequest newLocReq);
+    }
+}
