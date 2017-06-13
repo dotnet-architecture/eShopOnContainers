31fd93dafede7deecba5c24981146778a7b43685
Christian Arenas <carenas@plainconcepts.com>
2017-06-13 17:33:30 +0200

    Add Functional Test to marketing

---
+++ test/Services/FunctionalTests/Services/Marketing/MarketingScenariosBase.cs
@@ -0,0 +1,48 @@
+﻿namespace FunctionalTests.Services.Marketing
+{
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.TestHost;
+    using System.IO;
+
+    public class MarketingScenariosBase
+    {
+        public static string CampaignsUrlBase => "api/v1/campaigns";
+
+        public TestServer CreateServer()
+        {
+            var webHostBuilder = new WebHostBuilder();
+            webHostBuilder.UseContentRoot(Directory.GetCurrentDirectory() + "\\Services\\Marketing");
+            webHostBuilder.UseStartup<MarketingTestsStartup>();
+
+            return new TestServer(webHostBuilder);
+        }
+
+        public static class Get
+        {
+            public static string Campaigns = CampaignsUrlBase;
+
+            public static string CampaignBy(int id)
+                => $"{CampaignsUrlBase}/{id}";
+
+            public static string UserCampaignByUserId(System.Guid userId)
+                => $"{CampaignsUrlBase}/user/{userId}";
+        }
+
+        public static class Post
+        {
+            public static string AddNewCampaign = CampaignsUrlBase;
+        }
+
+        public static class Put
+        {
+            public static string CampaignBy(int id)
+                => $"{CampaignsUrlBase}/{id}";
+        }
+
+        public static class Delete
+        {
+            public static string CampaignBy(int id)
+                => $"{CampaignsUrlBase}/{id}";
+        }
+    }
+}
---
+++ test/Services/FunctionalTests/Services/Marketing/MarketingTestsStartup.cs
@@ -0,0 +1,26 @@
+﻿using FunctionalTests.Middleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.eShopOnContainers.Services.Ordering.API;
+
+namespace FunctionalTests.Services.Marketing
+{
+    public class MarketingTestsStartup : Startup
+    {
+        public MarketingTestsStartup(IHostingEnvironment env) : base(env)
+        {
+        }
+
+        protected override void ConfigureAuth(IApplicationBuilder app)
+        {
+            if (Configuration["isTest"] == bool.TrueString.ToLowerInvariant())
+            {
+                app.UseMiddleware<AutoAuthorizeMiddleware>();
+            }
+            else
+            {
+                base.ConfigureAuth(app);
+            }
+        }
+    }
+}
