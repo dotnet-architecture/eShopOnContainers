24c8e93888f37adb8a7c5f9e20ba270cd43f0b8d
Christian Arenas <carenas@plainconcepts.com>
2017-06-14 15:10:49 +0200

    Modify locationId in Marketing campaing seed

--- src/Services/Marketing/Marketing.API/Infrastructure/MarketingContextSeed.cs
+++ src/Services/Marketing/Marketing.API/Infrastructure/MarketingContextSeed.cs
@@ -57,7 +57,7 @@
                         new UserLocationRule
                         {
                             Description = "UserLocationRule2",
-                            LocationId = 3
+                            LocationId = 6
                         }
                     }
                 }
