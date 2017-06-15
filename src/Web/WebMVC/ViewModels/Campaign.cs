08a45f2c35c48011a48eef40300d0ad52ab478c9
Christian Arenas <carenas@plainconcepts.com>
2017-06-15 19:24:33 +0200

    Create Campaign view model and modify CampaignDTO

--- src/Web/WebMVC/ViewModels/Campaign.cs
+++ src/Web/WebMVC/ViewModels/Campaign.cs
@@ -1,8 +1,8 @@
-﻿namespace Microsoft.eShopOnContainers.WebMVC.Models
+﻿namespace Microsoft.eShopOnContainers.WebMVC.ViewModels
 {
     using System;
 
-    public class CampaignDTO
+    public class Campaign
     {
         public int Id { get; set; }
 
