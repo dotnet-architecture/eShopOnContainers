cae44bcbba22ec858340e00f687213633d5e5134
Christian Arenas <carenas@plainconcepts.com>
2017-06-21 11:06:33 +0200

    Add Barcelona location seed

--- src/Services/Location/Locations.API/Infrastructure/LocationsContextSeed.cs
+++ src/Services/Location/Locations.API/Infrastructure/LocationsContextSeed.cs
@@ -24,6 +24,7 @@
             {
                 await SetIndexes();
                 await SetUSLocations();
+                await SetBarcelonaLocations();
             }
         }
 
@@ -80,6 +81,20 @@
             rdm.SetArea(GetRedmondPoligon());
             await ctx.Locations.InsertOneAsync(rdm);
         }
+
+        static async Task SetBarcelonaLocations()
+        {
+            var bcn = new Locations()
+            {
+                Code = "BCN",
+                Description = "Barcelona",
+                LocationId = 5
+            };
+            bcn.SetLocation(2.156453, 41.395226);
+            bcn.SetArea(GetBarcelonaPoligon());
+            await ctx.Locations.InsertOneAsync(bcn);
+        }
+
 
         static async Task SetIndexes()
         {
@@ -140,6 +155,18 @@
                         new GeoJson2DGeographicCoordinates(-116.96912, 49.04282),
                         new GeoJson2DGeographicCoordinates(-124.68633, 48.8943)
                      };
+        }
+
+        static List<GeoJson2DGeographicCoordinates> GetBarcelonaPoligon()
+        {
+            return new List<GeoJson2DGeographicCoordinates>()
+            {
+                new GeoJson2DGeographicCoordinates(2.113741, 41.419068),
+                new GeoJson2DGeographicCoordinates(2.188778, 41.451153),
+                new GeoJson2DGeographicCoordinates(2.235266, 41.418033),
+                new GeoJson2DGeographicCoordinates(2.137101, 41.299536),
+                new GeoJson2DGeographicCoordinates(2.033879, 41.383858)
+            };
         }
     }
 }
