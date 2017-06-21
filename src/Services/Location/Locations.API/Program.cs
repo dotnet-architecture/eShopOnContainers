d35dc2ea17c420e12f8579ba3ee3cc1fe247d87b
IgorSychev <igorsychev@microsoft.com>
2017-06-21 21:02:27 +0300

    ApplicationInsights deleted from 3 projects.

--- src/Services/Location/Locations.API/Program.cs
+++ src/Services/Location/Locations.API/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 
@@ -17,7 +13,6 @@ namespace Microsoft.eShopOnContainers.Services.Locations.API
                 .UseHealthChecks("/hc")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
             host.Run();
