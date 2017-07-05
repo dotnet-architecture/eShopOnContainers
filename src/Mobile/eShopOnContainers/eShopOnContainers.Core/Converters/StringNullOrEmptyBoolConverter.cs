66ee0039e55af5044402378f79a7765a219fae40
Christian Arenas <carenas@plainconcepts.com>
2017-07-05 13:37:06 +0200

    Add StringNullOrEmptyBoolConverter to check when string value is null or empty

---
+++ src/Mobile/eShopOnContainers/eShopOnContainers.Core/Converters/StringNullOrEmptyBoolConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace eShopOnContainers.Core.Converters
+{
+    public class StringNullOrEmptyBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var stringValue = value as string;
+
+            return !string.IsNullOrEmpty(stringValue);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
