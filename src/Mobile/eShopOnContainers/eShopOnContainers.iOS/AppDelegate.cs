19663f7ba37509d710eb341a194aa95100032c88
David Britch <davidbritch@Davids-MBP.lan>
2017-08-10 15:38:04 +0100

    Removed Xamarin.Forms.Themes and Xamarin.Forms.DataPages NuGet packages in order to help speed up the app startup time #231

--- src/Mobile/eShopOnContainers/eShopOnContainers.iOS/AppDelegate.cs
+++ src/Mobile/eShopOnContainers/eShopOnContainers.iOS/AppDelegate.cs
@@ -4,28 +4,16 @@ using UIKit;
 
 namespace eShopOnContainers.iOS
 {
-    // The UIApplicationDelegate for the application. This class is responsible for launching the 
-    // User Interface of the application, as well as listening (and optionally responding) to 
-    // application events from iOS.
     [Register("AppDelegate")]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
-        //
-        // This method is invoked when the application has loaded and is ready to run. In this 
-        // method you should instantiate the window, load the UI into it and then make the window
-        // visible.
-        //
-        // You have 17 seconds to return from this method, or iOS will terminate your application.
-        //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
+
             SlideOverKit.iOS.SlideOverKit.Init();
             CachedImageRenderer.Init();
-
-            var x = typeof(Xamarin.Forms.Themes.LightThemeResources);
-            x = typeof(Xamarin.Forms.Themes.iOS.UnderlineEffect);
 
             return base.FinishedLaunching(app, options);
         }
