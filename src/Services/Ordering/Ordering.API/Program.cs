364c07a528f2c9d0b3be93127e335cdc953effcd
Ramón Tomás <ramon.tomas84@hotmail.com>
2017-08-29 12:03:20 +0200

    Included AspnetCore.all metapackage

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using System.IO;
 
 namespace Microsoft.eShopOnContainers.Services.Ordering.API
 {
@@ -13,6 +15,14 @@ namespace Microsoft.eShopOnContainers.Services.Ordering.API
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseHealthChecks("/hc")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureLogging((hostingContext, builder) =>
+                {
+                    builder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    builder.AddConsole();
+                    builder.AddDebug();
+                })
                 .Build();
     }
 }
