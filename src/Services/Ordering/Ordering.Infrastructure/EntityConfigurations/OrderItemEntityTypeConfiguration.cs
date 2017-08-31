70e429b76013b106df0c2a0dc12a0b7206836454
Unai Zorrilla Castro <Unai Zorrilla Castro>
2017-08-31 17:16:28 +0200

    Added IEntityTypeConfiguration feature from EF Core 2

---
+++ src/Services/Ordering/Ordering.Infrastructure/EntityConfigurations/OrderItemEntityTypeConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.eShopOnContainers.Services.Ordering.Domain.AggregatesModel.OrderAggregate;
+using Microsoft.eShopOnContainers.Services.Ordering.Infrastructure;
+
+namespace Ordering.Infrastructure.EntityConfigurations
+{
+    class OrderItemEntityTypeConfiguration
+        : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> orderItemConfiguration)
+        {
+            orderItemConfiguration.ToTable("orderItems", OrderingContext.DEFAULT_SCHEMA);
+
+            orderItemConfiguration.HasKey(o => o.Id);
+
+            orderItemConfiguration.Ignore(b => b.DomainEvents);
+
+            orderItemConfiguration.Property(o => o.Id)
+                .ForSqlServerUseSequenceHiLo("orderitemseq");
+
+            orderItemConfiguration.Property<int>("OrderId")
+                .IsRequired();
+
+            orderItemConfiguration.Property<decimal>("Discount")
+                .IsRequired();
+
+            orderItemConfiguration.Property<int>("ProductId")
+                .IsRequired();
+
+            orderItemConfiguration.Property<string>("ProductName")
+                .IsRequired();
+
+            orderItemConfiguration.Property<decimal>("UnitPrice")
+                .IsRequired();
+
+            orderItemConfiguration.Property<int>("Units")
+                .IsRequired();
+
+            orderItemConfiguration.Property<string>("PictureUrl")
+                .IsRequired(false);
+        }
+    }
+}
