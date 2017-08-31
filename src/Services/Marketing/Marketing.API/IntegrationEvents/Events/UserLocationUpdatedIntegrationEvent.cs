4a6fc5299a1544446e09af2962a58ff4d8ae7e1a
Unai Zorrilla Castro <Unai Zorrilla Castro>
2017-08-31 16:23:51 +0200

    Remove warning on usings in Marketing.API

--- src/Services/Marketing/Marketing.API/IntegrationEvents/Events/UserLocationUpdatedIntegrationEvent.cs
+++ src/Services/Marketing/Marketing.API/IntegrationEvents/Events/UserLocationUpdatedIntegrationEvent.cs
@@ -7,6 +7,7 @@
     public class UserLocationUpdatedIntegrationEvent : IntegrationEvent
     {
         public string UserId { get; set; }
+
         public List<UserLocationDetails> LocationList { get; set; }
 
         public UserLocationUpdatedIntegrationEvent(string userId, List<UserLocationDetails> locationList)
