86af9ec922cec680224711159b54f555186c4939
David Britch <davidbritch@Davids-MBP.lan>
2017-09-29 12:35:15 +0100

    New campaign icons for iOS + Android #353

--- src/Mobile/eShopOnContainers/eShopOnContainers.Droid/Resources/Resource.Designer.cs
+++ src/Mobile/eShopOnContainers/eShopOnContainers.Droid/Resources/Resource.Designer.cs
@@ -2281,82 +2281,85 @@ namespace eShopOnContainers.Droid
 			public const int icon = 2130837664;
 			
 			// aapt resource value: 0x7f0200a1
-			public const int menu_cart = 2130837665;
+			public const int menu_campaigns = 2130837665;
 			
 			// aapt resource value: 0x7f0200a2
-			public const int menu_filter = 2130837666;
+			public const int menu_cart = 2130837666;
 			
 			// aapt resource value: 0x7f0200a3
-			public const int menu_profile = 2130837667;
+			public const int menu_filter = 2130837667;
 			
 			// aapt resource value: 0x7f0200a4
-			public const int mr_dialog_material_background_dark = 2130837668;
+			public const int menu_profile = 2130837668;
 			
 			// aapt resource value: 0x7f0200a5
-			public const int mr_dialog_material_background_light = 2130837669;
+			public const int mr_dialog_material_background_dark = 2130837669;
 			
 			// aapt resource value: 0x7f0200a6
-			public const int mr_ic_audiotrack_light = 2130837670;
+			public const int mr_dialog_material_background_light = 2130837670;
 			
 			// aapt resource value: 0x7f0200a7
-			public const int mr_ic_cast_dark = 2130837671;
+			public const int mr_ic_audiotrack_light = 2130837671;
 			
 			// aapt resource value: 0x7f0200a8
-			public const int mr_ic_cast_light = 2130837672;
+			public const int mr_ic_cast_dark = 2130837672;
 			
 			// aapt resource value: 0x7f0200a9
-			public const int mr_ic_close_dark = 2130837673;
+			public const int mr_ic_cast_light = 2130837673;
 			
 			// aapt resource value: 0x7f0200aa
-			public const int mr_ic_close_light = 2130837674;
+			public const int mr_ic_close_dark = 2130837674;
 			
 			// aapt resource value: 0x7f0200ab
-			public const int mr_ic_media_route_connecting_mono_dark = 2130837675;
+			public const int mr_ic_close_light = 2130837675;
 			
 			// aapt resource value: 0x7f0200ac
-			public const int mr_ic_media_route_connecting_mono_light = 2130837676;
+			public const int mr_ic_media_route_connecting_mono_dark = 2130837676;
 			
 			// aapt resource value: 0x7f0200ad
-			public const int mr_ic_media_route_mono_dark = 2130837677;
+			public const int mr_ic_media_route_connecting_mono_light = 2130837677;
 			
 			// aapt resource value: 0x7f0200ae
-			public const int mr_ic_media_route_mono_light = 2130837678;
+			public const int mr_ic_media_route_mono_dark = 2130837678;
 			
 			// aapt resource value: 0x7f0200af
-			public const int mr_ic_pause_dark = 2130837679;
+			public const int mr_ic_media_route_mono_light = 2130837679;
 			
 			// aapt resource value: 0x7f0200b0
-			public const int mr_ic_pause_light = 2130837680;
+			public const int mr_ic_pause_dark = 2130837680;
 			
 			// aapt resource value: 0x7f0200b1
-			public const int mr_ic_play_dark = 2130837681;
+			public const int mr_ic_pause_light = 2130837681;
 			
 			// aapt resource value: 0x7f0200b2
-			public const int mr_ic_play_light = 2130837682;
+			public const int mr_ic_play_dark = 2130837682;
 			
 			// aapt resource value: 0x7f0200b3
-			public const int noimage = 2130837683;
-			
-			// aapt resource value: 0x7f0200ba
-			public const int notification_template_icon_bg = 2130837690;
+			public const int mr_ic_play_light = 2130837683;
 			
 			// aapt resource value: 0x7f0200b4
-			public const int product_add = 2130837684;
+			public const int noimage = 2130837684;
+			
+			// aapt resource value: 0x7f0200bb
+			public const int notification_template_icon_bg = 2130837691;
 			
 			// aapt resource value: 0x7f0200b5
-			public const int roundedbg = 2130837685;
+			public const int product_add = 2130837685;
 			
 			// aapt resource value: 0x7f0200b6
-			public const int roundedbgdark = 2130837686;
+			public const int roundedbg = 2130837686;
 			
 			// aapt resource value: 0x7f0200b7
-			public const int splash_drawable = 2130837687;
+			public const int roundedbgdark = 2130837687;
 			
 			// aapt resource value: 0x7f0200b8
-			public const int switch_off = 2130837688;
+			public const int splash_drawable = 2130837688;
 			
 			// aapt resource value: 0x7f0200b9
-			public const int switch_on = 2130837689;
+			public const int switch_off = 2130837689;
+			
+			// aapt resource value: 0x7f0200ba
+			public const int switch_on = 2130837690;
 			
 			static Drawable()
 			{
