771962f054aa71cbc7ceb2575d215b832cdedc58
David Britch <davidbritch@Davids-MBP.lan>
2017-10-25 16:56:38 +0100

    Removed unused methods.

--- src/Mobile/eShopOnContainers/eShopOnContainers.Core/Services/Order/IOrderService.cs
+++ src/Mobile/eShopOnContainers/eShopOnContainers.Core/Services/Order/IOrderService.cs
@@ -10,7 +10,6 @@ namespace eShopOnContainers.Core.Services.Order
         Task CreateOrderAsync(Models.Orders.Order newOrder, string token);
         Task<ObservableCollection<Models.Orders.Order>> GetOrdersAsync(string token);
         Task<Models.Orders.Order> GetOrderAsync(int orderId, string token);
-        Task<ObservableCollection<Models.Orders.CardType>> GetCardTypesAsync(string token);
         Task<bool> CancelOrderAsync(int orderId, string token);
         BasketCheckout MapOrderToBasket(Models.Orders.Order order);
     }
