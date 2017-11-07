625f53213bc48b6ba75f3e56c13f9d45e8c3fd46
Cesar De la Torre <cesardl@microsoft.com>
2017-11-07 12:45:05 -0800

    Added a comment about Addess as Value Object and EF Core 2.0 onwed entity

--- src/Services/Ordering/Ordering.Domain/AggregatesModel/OrderAggregate/Order.cs
+++ src/Services/Ordering/Ordering.Domain/AggregatesModel/OrderAggregate/Order.cs
@@ -15,6 +15,7 @@ namespace Microsoft.eShopOnContainers.Services.Ordering.Domain.AggregatesModel.O
         // aligned with DDD Aggregates and Domain Entities (Instead of properties and property collections)
         private DateTime _orderDate;
 
+        // Address is a Value Object pattern example persisted as EF Core 2.0 owned entity
         public Address Address { get; private set; }
 
         private int? _buyerId;
