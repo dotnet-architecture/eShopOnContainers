972ff42d94b631db403e4549493cbc1b80e6f8e4
David Britch <davidbritch@Davids-MacBook-Pro.local>
2018-01-10 17:41:34 +0000

    Changes to move the unit tests to .NET Standard.

---
+++ src/Mobile/eShopOnContainers/eShopOnContainers.UnitTests/Services/CatalogServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using eShopOnContainers.Core.Services.Catalog;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace eShopOnContainers.UnitTests
+{
+    public class CatalogServiceTests
+    {
+        [Fact]
+        public async Task GetFakeCatalogTest()
+        {
+            var catalogMockService = new CatalogMockService();
+            var catalog = await catalogMockService.GetCatalogAsync();
+
+            Assert.NotEqual(0, catalog.Count);
+        }
+
+        [Fact]
+        public async Task GetFakeCatalogBrandTest()
+        {
+            var catalogMockService = new CatalogMockService();
+            var catalogBrand = await catalogMockService.GetCatalogBrandAsync();
+
+            Assert.NotEqual(0, catalogBrand.Count);
+        }
+
+        [Fact]
+        public async Task GetFakeCatalogTypeTest()
+        {
+            var catalogMockService = new CatalogMockService();
+            var catalogType = await catalogMockService.GetCatalogTypeAsync();
+
+            Assert.NotEqual(0, catalogType.Count);
+        }
+    }
+}
