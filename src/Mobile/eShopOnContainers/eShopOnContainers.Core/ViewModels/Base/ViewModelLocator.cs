68d0783070dcbc84b7cc644ee0f638e7ceecfaed
David Britch <davidbritch@Davids-MacBook-Pro.local>
2018-02-19 16:59:29 +0000

    Removed TinyIoC behavior overrides.

--- src/Mobile/eShopOnContainers/eShopOnContainers.Core/ViewModels/Base/ViewModelLocator.cs
+++ src/Mobile/eShopOnContainers/eShopOnContainers.Core/ViewModels/Base/ViewModelLocator.cs
@@ -46,7 +46,7 @@ namespace eShopOnContainers.Core.ViewModels.Base
             _container.Register<BasketViewModel>();
             _container.Register<CatalogViewModel>();
             _container.Register<CheckoutViewModel>();
-            _container.Register<LoginViewModel>().AsSingleton();
+            _container.Register<LoginViewModel>();
             _container.Register<MainViewModel>();
             _container.Register<OrderDetailViewModel>();
             _container.Register<ProfileViewModel>();
@@ -59,7 +59,7 @@ namespace eShopOnContainers.Core.ViewModels.Base
             _container.Register<IDialogService, DialogService>();
             _container.Register<IOpenUrlService, OpenUrlService>();
             _container.Register<IIdentityService, IdentityService>();
-            _container.Register<IRequestProvider, RequestProvider>().AsMultiInstance();
+            _container.Register<IRequestProvider, RequestProvider>();
             _container.Register<IDependencyService, Services.Dependency.DependencyService>();
             _container.Register<ISettingsService, SettingsService>();
             _container.Register<IFixUriService, FixUriService>();
