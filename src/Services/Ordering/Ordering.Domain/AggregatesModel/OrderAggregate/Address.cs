0ee99022325f746f211e72d87e71bc7343e7c539
Enrico Tirotta <33745977+EnricoTirotta@users.noreply.github.com>
2018-03-03 08:41:35 +0100

    More protection from outside with guards
    I add a static constructor that take an input string. I try to put some guards in costructor with lower bound character for string input fields.
    Do you think I have a too defensive approach? Will be a good think add some custom meaningfull exception, not only Argument/InvalidOperation exceptions.

--- src/Services/Ordering/Ordering.Domain/AggregatesModel/OrderAggregate/Address.cs
+++ src/Services/Ordering/Ordering.Domain/AggregatesModel/OrderAggregate/Address.cs
@@ -1,26 +1,58 @@
-﻿using Microsoft.eShopOnContainers.Services.Ordering.Domain.SeedWork;
+using Microsoft.eShopOnContainers.Services.Ordering.Domain.SeedWork;
 using System;
 using System.Collections.Generic;
 
 namespace Microsoft.eShopOnContainers.Services.Ordering.Domain.AggregatesModel.OrderAggregate
 {
-    public class Address : ValueObject
+    public sealed class Address : ValueObject
     {
-        public String Street { get; private set; }
-        public String City { get; private set; }
-        public String State { get; private set; }
-        public String Country { get; private set; }
-        public String ZipCode { get; private set; }
+        private String Street;
+        private String City;
+        private String State;
+        private String Country;
+        private String ZipCode;
 
-        private Address() { }
-
-        public Address(string street, string city, string state, string country, string zipcode)
+        public static Address fromString(String address) 
         {
-            Street = street;
-            City = city;
-            State = state;
-            Country = country;
-            ZipCode = zipcode;
+            Guards.AssertStringIsNotEmptyOrNull(address);
+            Guards.AssertStringLowerBound(address,5);
+
+            //Address toString() format with '_' between fields
+            string[] addressElements = address.Split('_');
+            Guards.AssertIsTrue(addressElements.Length == 5, "Invalid Address string format");
+
+            //toString() order
+            String Street = addressElements[0];
+            String ZipCode = addressElements[1];
+            String City = addressElements[2];
+            String State = addressElements[3];
+            String Country = addressElements[4];
+
+            return new Address(Street, ZipCode, City, State, Country);
+        }
+
+        public Address(string street, string zipcode, string city, string state, string country)
+        {
+            Guards.AssertStringIsNotEmptyOrNull(street);
+            Guards.AssertStringLowerBound(street,3);
+            this.Street = street;
+
+            Guards.AssertStringIsNotEmptyOrNull(zipcode);
+            Guards.AssertStringLowerBound(zipcode, 3);
+            this.ZipCode = zipcode;
+
+            Guards.AssertStringIsNotEmptyOrNull(city);
+            Guards.AssertStringLowerBound(city, 3);
+            this.City = city;
+
+            Guards.AssertStringIsNotEmptyOrNull(state);
+            Guards.AssertStringLowerBound(state, 3);
+            this.State = state;
+
+            Guards.AssertStringIsNotEmptyOrNull(country);
+            Guards.AssertStringLowerBound(country, 3);
+            this.Country = country;
+
         }
 
         protected override IEnumerable<object> GetAtomicValues()
@@ -32,5 +64,9 @@ namespace Microsoft.eShopOnContainers.Services.Ordering.Domain.AggregatesModel.O
             yield return Country;
             yield return ZipCode;
         }
+
+        public override string ToString()
+        => $"{Street}_{ZipCode}_{City}_{State}_({Country})";
+
     }
 }
