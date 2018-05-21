460fc997af5bba130608bfe2a9d39811313a8f05
Unai Zorrilla Castro <unai@plainconcepts.com>
2018-05-21 15:01:29 +0200

    Create Generic Host prototype for background task in order to be reviewed

---
+++ src/Services/Ordering/Ordering.BackgroundTasksHost/Configuration/BackgroundTaskSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ordering.BackgroundTasksHost.Configuration
+{
+    public class BackgroundTaskSettings
+    {
+        public string ConnectionString { get; set; }
+
+        public string EventBusConnection { get; set; }
+
+        public int GracePeriodTime { get; set; }
+
+        public int CheckUpdateTime { get; set; }
+    }
+}
---
+++ src/Services/Ordering/Ordering.BackgroundTasksHost/IntegrationEvents/GracePeriodConfirmedIntegrationEvent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.eShopOnContainers.BuildingBlocks.EventBus.Events;
+
+namespace Ordering.BackgroundTasksHost.IntegrationEvents
+{
+    public class GracePeriodConfirmedIntegrationEvent : IntegrationEvent
+    {
+        public int OrderId { get; }
+
+        public GracePeriodConfirmedIntegrationEvent(int orderId) =>
+            OrderId = orderId;
+    }
+}
