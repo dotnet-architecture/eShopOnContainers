49200d77b221e9e82b1155f29208df69d70b137a
rafsanulhasan <rafsanulhasan@outlook.com>
2018-08-12 04:58:18 +0600

    Updated codes from net21rc1 branch

--- src/Web/WebMVC/ViewModels/Annotations/LongitudeCoordinate.cs
+++ src/Web/WebMVC/ViewModels/Annotations/LongitudeCoordinate.cs
@@ -3,20 +3,20 @@ using System.ComponentModel.DataAnnotations;
 
 namespace WebMVC.ViewModels.Annotations
 {
-    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = true)]
-    public class LongitudeCoordinate : ValidationAttribute
-    {
-        protected override ValidationResult
-                IsValid(object value, ValidationContext validationContext)
-        {
-            double coordinate;
-            if (!double.TryParse(value.ToString(), out coordinate) || (coordinate < -180 || coordinate > 180))
-            {
-                return new ValidationResult
-                    ("Longitude must be between -180 and 180 degrees inclusive.");
-            }
+	[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = true)]
+	public class LongitudeCoordinate : ValidationAttribute
+	{
+		protected override ValidationResult
+			   IsValid(object value, ValidationContext validationContext)
+		{
+			double coordinate;
+			if (!double.TryParse(value.ToString(), out coordinate) || (coordinate < -180 || coordinate > 180))
+			{
+				return new ValidationResult
+				    ("Longitude must be between -180 and 180 degrees inclusive.");
+			}
 
-            return ValidationResult.Success;            
-        }
-    }
+			return ValidationResult.Success;
+		}
+	}
 }
