8110a95111447413af0704e063f8dc8bd9bb1264
rafsanulhasan <rafsanulhasan@outlook.com>
2018-09-01 15:01:40 +0600

    1. Added _CookieConseentPartial.cshtml Partial View 2. Used _CookieConseentPartial partial view in _Layout View 3. Added Privacy View Controller and View

---
+++ src/Web/WebMVC/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
