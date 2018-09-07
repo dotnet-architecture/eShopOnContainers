f4d40d230f31c825b1c406186f9df86cc38b7ffc
rafsanulhasan <rafsanulhasan@outlook.com>
2018-09-07 19:39:51 +0600

    Simplify Namespace Name

--- src/Services/Ordering/Ordering.Domain/AggregatesModel/OrderAggregate/OrderStatus.cs
+++ src/Services/Ordering/Ordering.Domain/AggregatesModel/OrderAggregate/OrderStatus.cs
@@ -1,7 +1,7 @@
 ﻿namespace Microsoft.eShopOnContainers.Services.Ordering.Domain.AggregatesModel.OrderAggregate
 {
 	using global::Ordering.Domain.Exceptions;
-	using Microsoft.eShopOnContainers.Services.Ordering.Domain.SeedWork;
+	using SeedWork;
 	using System;
 	using System.Collections.Generic;
 	using System.Linq;
