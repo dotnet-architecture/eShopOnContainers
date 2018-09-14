e2121deeea84d3c68a7fa236668c1aa36c4b09e4
Nilesh Gule <vn_nilesh@yahoo.com>
2018-09-15 01:05:21 +0800

    Changed path separator to make powershell script work on Windows and Mac

--- k8s/deploy-ingress-azure.ps1
+++ k8s/deploy-ingress-azure.ps1
@@ -1,3 +1,3 @@
-﻿kubectl patch deployment -n ingress-nginx nginx-ingress-controller --type=json --patch="$(cat nginx-ingress\publish-service-patch.yaml)"
-kubectl apply -f nginx-ingress\azure\service.yaml
-kubectl apply -f nginx-ingress\patch-service-without-rbac.yaml
+﻿kubectl patch deployment -n ingress-nginx nginx-ingress-controller --type=json --patch="$(cat nginx-ingress/publish-service-patch.yaml)"
+kubectl apply -f nginx-ingress/azure/service.yaml
+kubectl apply -f nginx-ingress/patch-service-without-rbac.yaml
