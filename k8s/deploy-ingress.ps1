8b8fda1ab65522f1225af0b945c736291168dbd8
turibbio <turibbio@users.noreply.github.com>
2018-10-17 22:20:22 +0200

    Fix path separator on Mac OS x
    On Mac, the original script doesn't work. Thanks to $([IO.Path]::DirectorySeparatorChar), we can easily provide a standard way to execute correctly this script

--- k8s/deploy-ingress.ps1
+++ k8s/deploy-ingress.ps1
@@ -1,12 +1,12 @@
 kubectl apply -f ingress.yaml
 
 # Deploy nginx-ingress core files
-kubectl apply -f nginx-ingress\namespace.yaml
-kubectl apply -f nginx-ingress\default-backend.yaml
-kubectl apply -f nginx-ingress\configmap.yaml 
-kubectl apply -f nginx-ingress\tcp-services-configmap.yaml
-kubectl apply -f nginx-ingress\udp-services-configmap.yaml
-kubectl apply -f nginx-ingress\without-rbac.yaml
+kubectl apply -f nginx-ingress$([IO.Path]::DirectorySeparatorChar)namespace.yaml
+kubectl apply -f nginx-ingress$([IO.Path]::DirectorySeparatorChar)default-backend.yaml
+kubectl apply -f nginx-ingress$([IO.Path]::DirectorySeparatorChar)configmap.yaml 
+kubectl apply -f nginx-ingress$([IO.Path]::DirectorySeparatorChar)tcp-services-configmap.yaml
+kubectl apply -f nginx-ingress$([IO.Path]::DirectorySeparatorChar)udp-services-configmap.yaml
+kubectl apply -f nginx-ingress$([IO.Path]::DirectorySeparatorChar)without-rbac.yaml
 
 
 
