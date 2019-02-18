da72fc8cea44ce8263154e1986fceae4d58d2c12
Miguel Veloso <mvelosop@gmail.com>
2019-02-18 19:33:33 +0000

    Revert "Add Seq to ApiGatewayAggregators"
    This reverts commit 21427ebc3566b53cb53823b578917bf30f95e7ea.

--- src/ApiGateways/Mobile.Bff.Shopping/aggregator/Program.cs
+++ src/ApiGateways/Mobile.Bff.Shopping/aggregator/Program.cs
@@ -1,81 +1,44 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Logging;
 using Serilog;
-using System;
-using System.IO;
 
 namespace Microsoft.eShopOnContainers.Mobile.Shopping.HttpAggregator
 {
     public class Program
     {
-        public static readonly string AppName = typeof(Program).Namespace;
-        public static readonly string ShortAppName = AppName.Substring(AppName.LastIndexOf('.', AppName.LastIndexOf('.') - 1) + 1);
-
-        public static int Main(string[] args)
+        public static void Main(string[] args)
         {
-            var configuration = GetConfiguration();
-
-            Log.Logger = CreateSerilogLogger(configuration);
-
-            try
-            {
-                Log.Information("Configuring web host ({Application})...", AppName);
-                var host = BuildWebHost(configuration, args);
-
-                Log.Information("Starting web host ({Application})...", AppName);
-                host.Run();
-
-                return 0;
-            }
-            catch (Exception ex)
-            {
-                Log.Fatal(ex, "Program terminated unexpectedly ({Application})!", AppName);
-                return 1;
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
+            BuildWebHost(args).Run();
         }
 
-        private static IWebHost BuildWebHost(IConfiguration configuration, string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .CaptureStartupErrors(false)
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost
+                .CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(cb =>
+                {
+                    var sources = cb.Sources;
+                    sources.Insert(3, new Microsoft.Extensions.Configuration.Json.JsonConfigurationSource()
+                    {
+                        Optional = true,
+                        Path = "appsettings.localhost.json",
+                        ReloadOnChange = false
+                    });
+                })
                 .UseStartup<Startup>()
-                .UseSerilog()
+                .UseSerilog((builderContext, config) =>
+                {
+                    config
+                        .MinimumLevel.Information()
+                        .Enrich.FromLogContext()
+                        .WriteTo.Console();
+                })
                 .Build();
-
-        private static Serilog.ILogger CreateSerilogLogger(IConfiguration configuration)
-        {
-            var seqServerUrl = configuration["Serilog:SeqServerUrl"];
-
-            return new LoggerConfiguration()
-                .MinimumLevel.Verbose()
-                .Enrich.WithProperty("Application", AppName)
-                .Enrich.FromLogContext()
-                .WriteTo.Console()
-                .WriteTo.Seq(string.IsNullOrWhiteSpace(seqServerUrl) ? "http://seq" : seqServerUrl)
-                .ReadFrom.Configuration(configuration)
-                .CreateLogger();
-        }
-
-        private static IConfiguration GetConfiguration()
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddEnvironmentVariables();
-
-            builder.Sources.Insert(3, new JsonConfigurationSource()
-            {
-                Optional = true,
-                Path = "appsettings.localhost.json",
-                ReloadOnChange = false
-            });
-
-            return builder.Build();
-        }
     }
 }
