ca475905d9d32fa22ca3af9d74c7d3078aae3c28
Rafsanul Hasan <rafsanulhasan@outlook.com>
2019-04-04 12:02:31 +0600

    Update AuthorizeCheckOperationFilter.cs

--- src/ApiGateways/Web.Bff.Shopping/aggregator/Filters/AuthorizeCheckOperationFilter.cs
+++ src/ApiGateways/Web.Bff.Shopping/aggregator/Filters/AuthorizeCheckOperationFilter.cs
@@ -1,37 +1,37 @@
 ﻿namespace Microsoft.eShopOnContainers.Web.Shopping.HttpAggregator.Filters
 {
-	using Microsoft.AspNetCore.Authorization;
-	using Swashbuckle.AspNetCore.Swagger;
-	using Swashbuckle.AspNetCore.SwaggerGen;
-	using System.Collections.Generic;
-	using System.Linq;
+    using Microsoft.AspNetCore.Authorization;
+    using Swashbuckle.AspNetCore.Swagger;
+    using Swashbuckle.AspNetCore.SwaggerGen;
+    using System.Collections.Generic;
+    using System.Linq;
 
-	namespace Basket.API.Infrastructure.Filters
+    namespace Basket.API.Infrastructure.Filters
+    {
+        public class AuthorizeCheckOperationFilter : IOperationFilter
 	{
-		public class AuthorizeCheckOperationFilter : IOperationFilter
+	    public void Apply(Operation operation, OperationFilterContext context)
+	    {
+	        // Check for authorize attribute
+
+		var hasAuthorize = context.MethodInfo.DeclaringType.GetCustomAttributes(true)
+					  .Union(context.MethodInfo.GetCustomAttributes(true))
+					  .OfType<AuthorizeAttribute>().Any();
+
+		if (hasAuthorize)
 		{
-			public void Apply(Operation operation, OperationFilterContext context)
+		    operation.Responses.Add("401", new Response { Description = "Unauthorized" });
+		    operation.Responses.Add("403", new Response { Description = "Forbidden" });
+
+		    operation.Security = new List<IDictionary<string, IEnumerable<string>>>
+		    {
+			new Dictionary<string, IEnumerable<string>>
 			{
-				// Check for authorize attribute
-
-				var hasAuthorize = context.MethodInfo.DeclaringType.GetCustomAttributes(true)
-					                     .Union(context.MethodInfo.GetCustomAttributes(true))
-									 .OfType<AuthorizeAttribute>().Any();
-
-				if (hasAuthorize)
-				{
-					operation.Responses.Add("401", new Response { Description = "Unauthorized" });
-					operation.Responses.Add("403", new Response { Description = "Forbidden" });
-
-					operation.Security = new List<IDictionary<string, IEnumerable<string>>>
-					{
-						new Dictionary<string, IEnumerable<string>>
-						{
-							{ "oauth2", new [] { "Microsoft.eShopOnContainers.Web.Shopping.HttpAggregator" } }
-						}
-					};
-				}
+			    { "oauth2", new [] { "Microsoft.eShopOnContainers.Web.Shopping.HttpAggregator" } }
 			}
-		}
+		    };
+   	        }
+	    }
 	}
+    }
 }
