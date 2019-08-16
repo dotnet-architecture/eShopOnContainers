ddff0ce19e8aecb33a6f18eb45b343ac27034947
David Britch <davidbritch@users.noreply.github.com>
2019-08-16 17:11:54 +0100

    Update to Xamarin.Forms 4.1, with package refs.

--- src/Mobile/eShopOnContainers/eShopOnContainers.Droid/Renderers/SlideDownMenuPageRenderer.cs
+++ src/Mobile/eShopOnContainers/eShopOnContainers.Droid/Renderers/SlideDownMenuPageRenderer.cs
@@ -19,7 +19,7 @@ namespace eShopOnContainers.Droid.Renderers
 
         public SlideDownMenuPageRenderer(Context context) : base(context)
         {
-            new SlideOverKitDroidHandler().Init(this);
+            new SlideOverKitDroidHandler().Init(this, context);
         }
 
         protected override void OnElementChanged(ElementChangedEventArgs<Page> e)
