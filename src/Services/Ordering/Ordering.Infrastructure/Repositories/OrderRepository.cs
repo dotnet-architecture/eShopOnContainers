10759109aba67d4cead55848ededa1993a753733
ericuss <etorre@gmail.com>
2019-08-29 16:16:24 +0200

    temporal fix to get the order in local dbset of orders for UpdateOrderWhenBuyerAndPaymentMethodVerifiedDomainEventHandler.cs

--- src/Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -28,7 +28,7 @@ namespace Microsoft.eShopOnContainers.Services.Ordering.Infrastructure.Repositor
         public Order Add(Order order)
         {
             return  _context.Orders.Add(order).Entity;
-               
+
         }
 
         public async Task<Order> GetAsync(int orderId)
@@ -37,6 +37,14 @@ namespace Microsoft.eShopOnContainers.Services.Ordering.Infrastructure.Repositor
                                 .Orders
                                 .Include(x => x.Address)
                                 .FirstOrDefaultAsync(o => o.Id == orderId);
+            if (order == null)
+            {
+
+                order = _context
+                            .Orders
+                            .Local
+                            .FirstOrDefault(o => o.Id == orderId);
+            }
             if (order != null)
             {
                 await _context.Entry(order)
