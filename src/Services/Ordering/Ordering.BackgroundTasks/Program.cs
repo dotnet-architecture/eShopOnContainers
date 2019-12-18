a77b158d430b217d79f7d85912ba57756d345f65
Miguel Veloso <mvelosop@gmail.com>
2019-12-18 13:47:15 +0000

    Add back looging to Seq and Logstash

--- src/Services/Ordering/Ordering.BackgroundTasks/Program.cs
+++ src/Services/Ordering/Ordering.BackgroundTasks/Program.cs
@@ -1,5 +1,4 @@
 using Autofac.Extensions.DependencyInjection;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
