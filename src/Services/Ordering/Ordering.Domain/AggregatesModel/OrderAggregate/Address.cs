89c08382234e772ce8ab01abef15d88a1eeede06
André Silva <andrefilipegsilva@outlook.com>
2020-05-07 00:21:45 +0100

    refactored Equals() method on ValueObject

--- src/Services/Ordering/Ordering.Domain/AggregatesModel/OrderAggregate/Address.cs
+++ src/Services/Ordering/Ordering.Domain/AggregatesModel/OrderAggregate/Address.cs
@@ -23,7 +23,7 @@ namespace Microsoft.eShopOnContainers.Services.Ordering.Domain.AggregatesModel.O
             ZipCode = zipcode;
         }
 
-        protected override IEnumerable<object> GetAtomicValues()
+        protected override IEnumerable<object> GetEqualityComponents()
         {
             // Using a yield return statement to return each element one at a time
             yield return Street;
