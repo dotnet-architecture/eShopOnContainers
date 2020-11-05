9aeceb213b91c3e26ebe1b01d5209693111c5e2d
ansonzhang <3143422472@qq.com>
2020-11-05 09:06:23 +0800

    remove Ordering.API/ByPassAuthMid  duplicate cliam

--- src/Services/Ordering/Ordering.API/Infrastructure/Middlewares/ByPassAuthMiddleware.cs
+++ src/Services/Ordering/Ordering.API/Infrastructure/Middlewares/ByPassAuthMiddleware.cs
@@ -63,7 +63,6 @@ namespace Ordering.API.Infrastructure.Middlewares
                     new Claim("name", "Test user"),
                     new Claim("nonce", Guid.NewGuid().ToString()),
                     new Claim("http://schemas.microsoft.com/identity/claims/identityprovider", "ByPassAuthMiddleware"),
-                    new Claim("nonce", Guid.NewGuid().ToString()),
                     new Claim("sub", "1234"),
                     new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname","User"),
                     new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname","Microsoft")}
