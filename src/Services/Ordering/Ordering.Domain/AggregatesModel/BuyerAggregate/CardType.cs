2218a3d0ee6ad2b535a585c70ff7a080ac1aa0a4
Matt Callahan <matthew.callahan@outlook.com>
2020-11-25 07:40:07 -0700

    updated CardType and Enumeration classes

--- src/Services/Ordering/Ordering.Domain/AggregatesModel/BuyerAggregate/CardType.cs
+++ src/Services/Ordering/Ordering.Domain/AggregatesModel/BuyerAggregate/CardType.cs
@@ -9,9 +9,9 @@ namespace Microsoft.eShopOnContainers.Services.Ordering.Domain.AggregatesModel.B
     public class CardType
         : Enumeration
     {
-        public static CardType Amex = new CardType(1, "Amex");
-        public static CardType Visa = new CardType(2, "Visa");
-        public static CardType MasterCard = new CardType(3, "MasterCard");
+        public static CardType Amex = new CardType(1, nameof(Amex));
+        public static CardType Visa = new CardType(2, nameof(Visa));
+        public static CardType MasterCard = new CardType(3, nameof(MasterCard));
 
         public CardType(int id, string name)
             : base(id, name)
