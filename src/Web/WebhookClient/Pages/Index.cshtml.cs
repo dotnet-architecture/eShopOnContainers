89bf5efca0b50f06dea1ff8e2e20d5409caf62a9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-12-23 16:00:21 +0530

    Small Refactoring

--- src/Web/WebhookClient/Pages/Index.cshtml.cs
+++ src/Web/WebhookClient/Pages/Index.cshtml.cs
@@ -1,10 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Http;
 using WebhookClient.Models;
 using WebhookClient.Services;
 
