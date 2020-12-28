81e4312cdd8e297dd07620c64b5898a30c436476
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-12-28 15:10:01 +0530

    Small Refactoring (#1558)

--- src/Web/WebhookClient/Program.cs
+++ src/Web/WebhookClient/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace WebhookClient
 {
