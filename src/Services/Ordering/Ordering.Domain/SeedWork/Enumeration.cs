a71b0048970cab3e95b1f01297b3ded703a0a6d6
Matt Callahan <matthew.callahan@outlook.com>
2021-04-07 23:16:11 -0700

    updated CardType and Enumeration classes (#1528)
    * updated CardType and Enumeration classes
    
    * Update per IEvangelist's suggestion
    
    * Update src/Services/Ordering/Ordering.Domain/SeedWork/Enumeration.cs
    
    Co-authored-by: David Pine <david.pine@microsoft.com>
    
    Co-authored-by: Sumit Ghosh <sumit.ghosh@neudesic.com>
    Co-authored-by: David Pine <david.pine@microsoft.com>

--- src/Services/Ordering/Ordering.Domain/SeedWork/Enumeration.cs
+++ src/Services/Ordering/Ordering.Domain/SeedWork/Enumeration.cs
@@ -11,27 +11,23 @@ namespace Microsoft.eShopOnContainers.Services.Ordering.Domain.SeedWork
 
         public int Id { get; private set; }
 
-        protected Enumeration(int id, string name)
-        {
-            Id = id;
-            Name = name;
-        }
+        protected Enumeration(int id, string name) => (Id, Name) = (id, name);
 
         public override string ToString() => Name;
-
-        public static IEnumerable<T> GetAll<T>() where T : Enumeration
-        {
-            var fields = typeof(T).GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly);
-
-            return fields.Select(f => f.GetValue(null)).Cast<T>();
-        }
-
+        
+        public static IEnumerable<T> GetAll<T>() where T : Enumeration =>
+            typeof(T).GetFields(BindingFlags.Public |
+                                BindingFlags.Static |
+                                BindingFlags.DeclaredOnly)
+                     .Select(f => f.GetValue(null))
+                     .Cast<T>();
+        
         public override bool Equals(object obj)
         {
-            var otherValue = obj as Enumeration;
-
-            if (otherValue == null)
+            if (obj is not Enumeration otherValue)
+            {
                 return false;
+            }
 
             var typeMatches = GetType().Equals(obj.GetType());
             var valueMatches = Id.Equals(otherValue.Id);
