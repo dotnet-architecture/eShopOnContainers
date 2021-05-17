bf7ade80d286f5d0a6c1eaa88a1aad9764a935aa
David Pine <david.pine@microsoft.com>
2021-05-17 02:46:38 -0500

    Update CardType.cs (#1674)
    Sync with https://github.com/dotnet/docs/pull/24236

--- src/Services/Ordering/Ordering.Domain/AggregatesModel/BuyerAggregate/CardType.cs
+++ src/Services/Ordering/Ordering.Domain/AggregatesModel/BuyerAggregate/CardType.cs
@@ -9,9 +9,9 @@ namespace Microsoft.eShopOnContainers.Services.Ordering.Domain.AggregatesModel.B
     public class CardType
         : Enumeration
     {
-        public static CardType Amex = new CardType(1, nameof(Amex));
-        public static CardType Visa = new CardType(2, nameof(Visa));
-        public static CardType MasterCard = new CardType(3, nameof(MasterCard));
+        public static CardType Amex = new(1, nameof(Amex));
+        public static CardType Visa = new(2, nameof(Visa));
+        public static CardType MasterCard = new(3, nameof(MasterCard));
 
         public CardType(int id, string name)
             : base(id, name)
