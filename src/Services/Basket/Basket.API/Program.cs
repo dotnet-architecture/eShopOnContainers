d4d784c522ca4d8d617238872a47afd7a2bc86a6
zedy <81678720+zedy-wj@users.noreply.github.com>
2021-05-28 11:37:34 +0800

    Update Program.cs
    Modify format

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -89,8 +89,7 @@ IConfiguration GetConfiguration()
 
     if (config.GetValue<bool>("UseVault", false))
     {
-        builder.AddAzureKeyVault(
-            new Uri($"https://{config["Vault:Name"]}.vault.azure.net/"),new DefaultAzureCredential());
+        builder.AddAzureKeyVault(new Uri($"https://{config["Vault:Name"]}.vault.azure.net/"),new DefaultAzureCredential());
     }
 
     return builder.Build();
