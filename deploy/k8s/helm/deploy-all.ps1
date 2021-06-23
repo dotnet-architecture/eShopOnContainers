3bd338a3a96985e2f711d16439fa37e5364fd235
Mauricio Caro <maucaro@outlook.com>
2021-06-22 17:26:35 -0700

    Local K8s change DNS to host.docker.internal

--- deploy/k8s/helm/deploy-all.ps1
+++ deploy/k8s/helm/deploy-all.ps1
@@ -61,7 +61,7 @@ $ingressValuesFile="ingress_values.yaml"
 
 if ($useLocalk8s -eq $true) {
     $ingressValuesFile="ingress_values_dockerk8s.yaml"
-    $dns="localhost"
+    $dns="host.docker.internal"
 }
 
 if ($externalDns -eq "aks") {
