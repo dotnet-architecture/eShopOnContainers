76ddee775604a946eb3d087d8df3e154ca8d527b
Sumit Ghosh <sumit.ghosh@neudesic.com>
2021-08-12 18:56:38 +0530

    Use file-scoped namespaces

--- src/Services/Basket/Basket.API/Infrastructure/Middlewares/FailingStartupFilter.cs
+++ src/Services/Basket/Basket.API/Infrastructure/Middlewares/FailingStartupFilter.cs
@@ -1,24 +1,20 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using System;
+﻿namespace Basket.API.Infrastructure.Middlewares;
 
-namespace Basket.API.Infrastructure.Middlewares
+public class FailingStartupFilter : IStartupFilter
 {
-    public class FailingStartupFilter : IStartupFilter
+    private readonly Action<FailingOptions> _options;
+    public FailingStartupFilter(Action<FailingOptions> optionsAction)
     {
-        private readonly Action<FailingOptions> _options;
-        public FailingStartupFilter(Action<FailingOptions> optionsAction)
-        {
-            _options = optionsAction;
-        }
+        _options = optionsAction;
+    }
 
-        public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+    public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+    {
+        return app =>
         {
-            return app =>
-            {
-                app.UseFailingMiddleware(_options);
-                next(app);
-            };
-        }
+            app.UseFailingMiddleware(_options);
+            next(app);
+        };
     }
 }
+
--- src/Services/Basket/Basket.API/Model/CustomerBasket.cs
+++ src/Services/Basket/Basket.API/Model/CustomerBasket.cs
@@ -1,21 +1,18 @@
-﻿using System.Collections.Generic;
-
-namespace Microsoft.eShopOnContainers.Services.Basket.API.Model
+﻿namespace Microsoft.eShopOnContainers.Services.Basket.API.Model;
+public class CustomerBasket
 {
-    public class CustomerBasket
+    public string BuyerId { get; set; }
+
+    public List<BasketItem> Items { get; set; } = new List<BasketItem>();
+
+    public CustomerBasket()
     {
-        public string BuyerId { get; set; }
 
-        public List<BasketItem> Items { get; set; } = new List<BasketItem>();
+    }
 
-        public CustomerBasket()
-        {
-
-        }
-
-        public CustomerBasket(string customerId)
-        {
-            BuyerId = customerId;
-        }
+    public CustomerBasket(string customerId)
+    {
+        BuyerId = customerId;
     }
 }
+
