409fab7521c702cc031686404a9e9a02e7afa039
Sumit Ghosh <sumit.ghosh@neudesic.com>
2021-10-13 16:37:33 +0530

    Included global using in EventBusServiceBus project

---
+++ src/BuildingBlocks/EventBus/EventBusServiceBus/GlobalUsings.cs
@@ -0,0 +1,22 @@
+﻿global using Microsoft.eShopOnContainers.BuildingBlocks.EventBus.Abstractions;
+global using Microsoft.eShopOnContainers.BuildingBlocks.EventBus.Events;
+global using static Microsoft.eShopOnContainers.BuildingBlocks.EventBus.InMemoryEventBusSubscriptionsManager;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text.Json.Serialization;
+global using System.Threading.Tasks;
+global using System;
+global using Microsoft.Azure.ServiceBus;
+global using Autofac;
+global using Microsoft.eShopOnContainers.BuildingBlocks.EventBus;
+global using Microsoft.Extensions.Logging;
+global using System.Text;
+global using System.Text.Json;
+
+
+
+
+
+
+
+
