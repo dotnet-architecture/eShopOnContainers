9d73669d861ae4b32ebadd738718bb7cbfb13161
Sumit Ghosh <sumit.ghosh@neudesic.com>
2021-10-13 17:37:01 +0530

    Move using statements to globalusing file

--- src/BuildingBlocks/EventBus/IntegrationEventLogEF/IntegrationEventLogContext.cs
+++ src/BuildingBlocks/EventBus/IntegrationEventLogEF/IntegrationEventLogContext.cs
@@ -1,45 +1,41 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
+﻿namespace Microsoft.eShopOnContainers.BuildingBlocks.IntegrationEventLogEF;
 
-namespace Microsoft.eShopOnContainers.BuildingBlocks.IntegrationEventLogEF
+public class IntegrationEventLogContext : DbContext
 {
-    public class IntegrationEventLogContext : DbContext
+    public IntegrationEventLogContext(DbContextOptions<IntegrationEventLogContext> options) : base(options)
     {
-        public IntegrationEventLogContext(DbContextOptions<IntegrationEventLogContext> options) : base(options)
-        {
-        }
+    }
 
-        public DbSet<IntegrationEventLogEntry> IntegrationEventLogs { get; set; }
+    public DbSet<IntegrationEventLogEntry> IntegrationEventLogs { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            builder.Entity<IntegrationEventLogEntry>(ConfigureIntegrationEventLogEntry);
-        }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<IntegrationEventLogEntry>(ConfigureIntegrationEventLogEntry);
+    }
 
-        void ConfigureIntegrationEventLogEntry(EntityTypeBuilder<IntegrationEventLogEntry> builder)
-        {
-            builder.ToTable("IntegrationEventLog");
+    void ConfigureIntegrationEventLogEntry(EntityTypeBuilder<IntegrationEventLogEntry> builder)
+    {
+        builder.ToTable("IntegrationEventLog");
 
-            builder.HasKey(e => e.EventId);
+        builder.HasKey(e => e.EventId);
 
-            builder.Property(e => e.EventId)
-                .IsRequired();
+        builder.Property(e => e.EventId)
+            .IsRequired();
 
-            builder.Property(e => e.Content)
-                .IsRequired();
+        builder.Property(e => e.Content)
+            .IsRequired();
 
-            builder.Property(e => e.CreationTime)
-                .IsRequired();
+        builder.Property(e => e.CreationTime)
+            .IsRequired();
 
-            builder.Property(e => e.State)
-                .IsRequired();
+        builder.Property(e => e.State)
+            .IsRequired();
 
-            builder.Property(e => e.TimesSent)
-                .IsRequired();
+        builder.Property(e => e.TimesSent)
+            .IsRequired();
 
-            builder.Property(e => e.EventTypeName)
-                .IsRequired();
+        builder.Property(e => e.EventTypeName)
+            .IsRequired();
 
-        }
     }
 }
