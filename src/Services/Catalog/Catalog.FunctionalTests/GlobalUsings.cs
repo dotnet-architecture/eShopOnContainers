b772a8363f0b9298ee1e4aa7b7395c57c06c9546
Sumit Ghosh <sumit.ghosh@neudesic.com>
2021-10-14 16:36:39 +0530

    Included global usings for Catalog.FunctionalTests

---
+++ src/Services/Catalog/Catalog.FunctionalTests/GlobalUsings.cs
@@ -0,0 +1,16 @@
+﻿global using Catalog.API.Extensions;
+global using Microsoft.AspNetCore.Hosting;
+global using Microsoft.AspNetCore.TestHost;
+global using Microsoft.eShopOnContainers.BuildingBlocks.IntegrationEventLogEF;
+global using Microsoft.eShopOnContainers.Services.Catalog.API.Infrastructure;
+global using Microsoft.eShopOnContainers.Services.Catalog.API;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
+global using Microsoft.Extensions.Options;
+global using System.IO;
+global using System.Net;
+global using System.Reflection;
+global using System.Threading.Tasks;
+global using Xunit;
