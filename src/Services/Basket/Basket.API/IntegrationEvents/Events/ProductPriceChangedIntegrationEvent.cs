5359ddd84ab242148cccf4b8df4313dd9e245edb
Ryan Celeslie <20121443+ryanceleslie@users.noreply.github.com>
2021-10-26 16:55:05 -0400

    Updated comment
    Comment was missing the last part of the sentence

--- src/Services/Basket/Basket.API/IntegrationEvents/Events/ProductPriceChangedIntegrationEvent.cs
+++ src/Services/Basket/Basket.API/IntegrationEvents/Events/ProductPriceChangedIntegrationEvent.cs
@@ -3,7 +3,7 @@
 namespace Microsoft.eShopOnContainers.Services.Basket.API.IntegrationEvents.Events
 {
     // Integration Events notes: 
-    // An Event is “something that has happened in the past”, therefore its name has to be   
+    // An Event is “something that has happened in the past”, therefore its name has to be past tense
     // An Integration Event is an event that can cause side effects to other microsrvices, Bounded-Contexts or external systems.
     public record ProductPriceChangedIntegrationEvent : IntegrationEvent
     {
