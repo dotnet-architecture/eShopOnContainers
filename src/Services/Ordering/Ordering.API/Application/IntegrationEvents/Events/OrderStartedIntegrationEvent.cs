346bab22254c4ca4074008e0586ff9dc77eb9375
chertby <a.skuratovich@gmail.com>
2021-12-07 12:22:32 +0300

    docs: Fix typo in comments (#1796)

--- src/Services/Ordering/Ordering.API/Application/IntegrationEvents/Events/OrderStartedIntegrationEvent.cs
+++ src/Services/Ordering/Ordering.API/Application/IntegrationEvents/Events/OrderStartedIntegrationEvent.cs
@@ -1,8 +1,8 @@
 ﻿namespace Microsoft.eShopOnContainers.Services.Ordering.API.Application.IntegrationEvents.Events;
 
-// Integration Events notes: 
-// An Event is “something that has happened in the past”, therefore its name has to be   
-// An Integration Event is an event that can cause side effects to other microsrvices, Bounded-Contexts or external systems.
+// Integration Events notes:
+// An Event is “something that has happened in the past”, therefore its name has to be
+// An Integration Event is an event that can cause side effects to other microservices, Bounded-Contexts or external systems.
 public record OrderStartedIntegrationEvent : IntegrationEvent
 {
     public string UserId { get; init; }
