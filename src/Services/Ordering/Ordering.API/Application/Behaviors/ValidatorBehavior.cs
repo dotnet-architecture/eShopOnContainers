f7a3aaec545efdb9ea1b282758591a081a5d3ea7
A-Soltani <soltani1384@gmail.com>
2021-12-20 08:55:38 +0330

    fix constructor injection for _validators

--- src/Services/Ordering/Ordering.API/Application/Behaviors/ValidatorBehavior.cs
+++ src/Services/Ordering/Ordering.API/Application/Behaviors/ValidatorBehavior.cs
@@ -3,9 +3,9 @@
 public class ValidatorBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
 {
     private readonly ILogger<ValidatorBehavior<TRequest, TResponse>> _logger;
-    private readonly IValidator<TRequest>[] _validators;
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
 
-    public ValidatorBehavior(IValidator<TRequest>[] validators, ILogger<ValidatorBehavior<TRequest, TResponse>> logger)
+    public ValidatorBehavior(IEnumerable<IValidator<TRequest>> validators, ILogger<ValidatorBehavior<TRequest, TResponse>> logger)
     {
         _validators = validators;
         _logger = logger;
