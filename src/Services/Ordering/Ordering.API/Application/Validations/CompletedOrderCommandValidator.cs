73400df12b1a21912d66ece3f1116b286a671af8
cemaleddin <cemaleddinkuru@gmail.com>
2022-07-23 23:58:34 +0300

    order status change for add validation

---
+++ src/Services/Ordering/Ordering.API/Application/Validations/CompletedOrderCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿namespace Microsoft.eShopOnContainers.Services.Ordering.API.Application.Validations;
+
+public class CompletedOrderCommandValidator : AbstractValidator<SetCompletedOrderStatusCommand>
+{
+    public CompletedOrderCommandValidator(ILogger<CompletedOrderCommandValidator> logger)
+    {
+        RuleFor(order => order.OrderNumber).NotEmpty().WithMessage("No orderId found");      
+
+        logger.LogTrace("----- INSTANCE CREATED - {ClassName}", GetType().Name);
+    }
+}
