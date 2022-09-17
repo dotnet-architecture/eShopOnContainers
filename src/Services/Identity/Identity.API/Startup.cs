0c64635c89f1b393452429526d5346bc307009f7
Marek Wilkosz <AMW044@motorolasolutions.com>
2022-09-17 12:36:28 +0200

    fixed csp for local

--- src/Services/Identity/Identity.API/Startup.cs
+++ src/Services/Identity/Identity.API/Startup.cs
@@ -121,10 +121,10 @@ namespace Microsoft.eShopOnContainers.Services.Identity.API
 
             app.UseStaticFiles();
 
-            // Make work identity server redirections in Edge and lastest versions of browsers. WARN: Not valid in a production environment.
+            // TODO Make work identity server redirections in Edge and lastest versions of browsers. WARN: Not valid in a production environment.
             app.Use(async (context, next) =>
             {
-                context.Response.Headers.Add("Content-Security-Policy", "script-src 'unsafe-inline'");
+                context.Response.Headers.Add("Content-Security-Policy", "script-src 'self' http://host.docker.internal:5105; script-src-elem 'self' http://host.docker.internal:5105");
                 await next();
             });
 
