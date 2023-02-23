47f9559de043966158c8417c01df454bfad1adfe
Tarun Jain <v-tjain@microsoft.com>
2023-02-23 16:20:31 +0530

    Commit to migrate angular version to latest '15.x'

--- src/Web/WebSPA/Client/src/test.ts
+++ src/Web/WebSPA/Client/src/test.ts
@@ -14,7 +14,6 @@ import {
 
 // Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
 declare var __karma__: any;
-declare var require: any;
 
 // Prevent Karma from running prematurely.
 __karma__.loaded = function () {};
@@ -26,9 +25,5 @@ getTestBed().initTestEnvironment(
     teardown: { destroyAfterEach: false }
 }
 );
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
 // Finally, start Karma to run the tests.
 __karma__.start();
