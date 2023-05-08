72c8167ec061bfb5f6a8c44f0ed32b3960612b53
Reuben Bond <reuben.bond@gmail.com>
2023-05-08 11:24:04 -0700

    Attempt to migrate Mobile BFF to common services pattern

--- src/ApiGateways/Mobile.Bff.Shopping/aggregator/Program.cs
+++ src/ApiGateways/Mobile.Bff.Shopping/aggregator/Program.cs
@@ -1,145 +1,38 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddHealthChecks()
-    .AddCheck("self", () => HealthCheckResult.Healthy())
-    .AddUrlGroup(new Uri(builder.Configuration["CatalogUrlHC"]), name: "catalogapi-check", tags: new string[] { "catalogapi" })
-    .AddUrlGroup(new Uri(builder.Configuration["OrderingUrlHC"]), name: "orderingapi-check", tags: new string[] { "orderingapi" })
-    .AddUrlGroup(new Uri(builder.Configuration["BasketUrlHC"]), name: "basketapi-check", tags: new string[] { "basketapi" })
-    .AddUrlGroup(new Uri(builder.Configuration["IdentityUrlHC"]), name: "identityapi-check", tags: new string[] { "identityapi" })
-    .AddUrlGroup(new Uri(builder.Configuration["PaymentUrlHC"]), name: "paymentapi-check", tags: new string[] { "paymentapi" });
+builder.AddServiceDefaults();
 
-builder.Services.Configure<UrlsConfig>(builder.Configuration.GetSection("urls"));
+builder.Services.AddReverseProxy(builder.Configuration);
+builder.Services.AddControllers();
 
-builder.Services.AddControllers()
-        .AddJsonOptions(options => options.JsonSerializerOptions.WriteIndented = true);
-
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "Shopping Aggregator for Mobile Clients",
-        Version = "v1",
-        Description = "Shopping Aggregator for Mobile Clients"
-    });
-    var identityUrl = builder.Configuration.GetSection("Identity").GetValue<string>("ExternalUrl");
-    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
-    {
-        Type = SecuritySchemeType.OAuth2,
-        Flows = new OpenApiOAuthFlows()
-        {
-            Implicit = new OpenApiOAuthFlow()
-            {
-                AuthorizationUrl = new Uri($"{identityUrl}/connect/authorize"),
-                TokenUrl = new Uri($"{identityUrl}/connect/token"),
-
-                Scopes = new Dictionary<string, string>()
-                {
-                    { "mobileshoppingagg", "Shopping Aggregator for Mobile Clients" }
-                }
-            }
-        }
-    });
-
-    options.OperationFilter<AuthorizeCheckOperationFilter>();
-});
-
+builder.Services.AddHealthChecks(builder.Configuration);
 builder.Services.AddCors(options =>
 {
+    // TODO: Read allowed origins from configuration
     options.AddPolicy("CorsPolicy",
         builder => builder
+        .SetIsOriginAllowed((host) => true)
         .AllowAnyMethod()
         .AllowAnyHeader()
-        .SetIsOriginAllowed((host) => true)
         .AllowCredentials());
 });
 
-JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+builder.Services.AddApplicationServices();
+builder.Services.AddGrpcServices();
 
-var identityUrl = builder.Configuration.GetValue<string>("urls:identity");
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-
-})
-.AddJwtBearer(options =>
-{
-    options.Authority = identityUrl;
-    options.RequireHttpsMetadata = false;
-    options.Audience = "mobileshoppingagg";
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateAudience = false
-    };
-});
-
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("ApiScope", policy =>
-    {
-        policy.RequireAuthenticatedUser();
-        policy.RequireClaim("scope", "mobileshoppingagg");
-    });
-});
-
-builder.Services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
-builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-builder.Services.AddHttpClient<IOrderApiClient, OrderApiClient>();
-
-builder.Services.AddTransient<GrpcExceptionInterceptor>();
-builder.Services.AddScoped<IBasketService, BasketService>();
-builder.Services.AddGrpcClient<Basket.BasketClient>((services, options) =>
-{
-    var basketApi = services.GetRequiredService<IOptions<UrlsConfig>>().Value.GrpcBasket;
-    options.Address = new Uri(basketApi);
-}).AddInterceptor<GrpcExceptionInterceptor>();
-builder.Services.AddScoped<ICatalogService, CatalogService>();
-builder.Services.AddGrpcClient<Catalog.CatalogClient>((services, options) =>
-{
-    var catalogApi = services.GetRequiredService<IOptions<UrlsConfig>>().Value.GrpcCatalog;
-    options.Address = new Uri(catalogApi);
-}).AddInterceptor<GrpcExceptionInterceptor>();
-builder.Services.AddScoped<IOrderingService, OrderingService>();
-builder.Services.AddGrpcClient<GrpcOrdering.OrderingGrpc.OrderingGrpcClient>((services, options) =>
-{
-    var orderingApi = services.GetRequiredService<IOptions<UrlsConfig>>().Value.GrpcOrdering;
-    options.Address = new Uri(orderingApi);
-}).AddInterceptor<GrpcExceptionInterceptor>();
+builder.Services.Configure<UrlsConfig>(builder.Configuration.GetSection("urls"));
 
 var app = builder.Build();
 
-var pathBase = app.Configuration["PATH_BASE"];
+app.UseServiceDefaults();
 
-if (!string.IsNullOrEmpty(pathBase))
-{
-    app.UsePathBase(pathBase);
-}
+app.UseHttpsRedirection();
 
-app.UseSwagger().UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint($"{(!string.IsNullOrEmpty(pathBase) ? pathBase : string.Empty)}/swagger/v1/swagger.json", "Purchase BFF V1");
-
-    c.OAuthClientId("mobileshoppingaggswaggerui");
-    c.OAuthClientSecret(string.Empty);
-    c.OAuthRealm(string.Empty);
-    c.OAuthAppName("Purchase BFF Swagger UI");
-});
-
-app.UseRouting();
 app.UseCors("CorsPolicy");
 app.UseAuthentication();
 app.UseAuthorization();
-app.MapDefaultControllerRoute();
+
 app.MapControllers();
-app.MapHealthChecks("/hc", new HealthCheckOptions()
-{
-    Predicate = _ => true,
-    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-});
-app.MapHealthChecks("/liveness", new HealthCheckOptions
-{
-    Predicate = r => r.Name.Contains("self")
-});
+app.MapReverseProxy();
 
 await app.RunAsync();
