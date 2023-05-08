dbef61fdafa3fedc68373781d2e1e88dc02cf3da
David Fowler <davidfowl@gmail.com>
2023-05-08 13:54:31 -0700

    Nuke the cors from the mobile BFF

--- src/ApiGateways/Mobile.Bff.Shopping/aggregator/Program.cs
+++ src/ApiGateways/Mobile.Bff.Shopping/aggregator/Program.cs
@@ -6,16 +6,6 @@ builder.Services.AddReverseProxy(builder.Configuration);
 builder.Services.AddControllers();
 
 builder.Services.AddHealthChecks(builder.Configuration);
-builder.Services.AddCors(options =>
-{
-    // TODO: Read allowed origins from configuration
-    options.AddPolicy("CorsPolicy",
-        builder => builder
-        .SetIsOriginAllowed((host) => true)
-        .AllowAnyMethod()
-        .AllowAnyHeader()
-        .AllowCredentials());
-});
 
 builder.Services.AddApplicationServices();
 builder.Services.AddGrpcServices();
@@ -27,10 +17,6 @@ var app = builder.Build();
 app.UseServiceDefaults();
 
 app.UseHttpsRedirection();
-
-app.UseCors("CorsPolicy");
-app.UseAuthentication();
-app.UseAuthorization();
 
 app.MapControllers();
 app.MapReverseProxy();
