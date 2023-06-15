493031b11c7cd23c742582cfc504a12c43438dd6
David Fowler <davidfowl@gmail.com>
2023-06-14 21:37:53 -0700

    Added implict usings

--- src/Tests/Services/Application.FunctionalTests/GlobalUsings.cs
+++ src/Tests/Services/Application.FunctionalTests/GlobalUsings.cs
@@ -1,14 +1,14 @@
 ﻿global using System;
-global using System.Net.Http;
-global using Microsoft.AspNetCore.Http;
-global using System.Security.Claims;
-global using System.Threading.Tasks;
-global using Microsoft.eShopOnContainers.Services.Basket.API.Model;
-global using Microsoft.eShopOnContainers.WebMVC.ViewModels;
-global using System.Text.Json;
 global using System.Collections.Generic;
 global using System.Linq;
+global using System.Net.Http;
+global using System.Security.Claims;
 global using System.Text;
+global using System.Text.Json;
+global using System.Threading.Tasks;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.eShopOnContainers.Services.Basket.API.Model;
+global using Microsoft.eShopOnContainers.WebMVC.ViewModels;
 global using WebMVC.Services.ModelDTOs;
 global using Xunit;
 
