7a03550f9b7763af425df1224d6ec462cddd7728
David Fowler <davidfowl@gmail.com>
2023-06-15 07:37:46 -0700

    Fixed the spa project - Clean up the SPA project (removed dead code) - Fixed URLs in override file

---
+++ src/Web/WebSPA/Extensions/Extensions.cs
@@ -0,0 +1,12 @@
+﻿internal static class Extensions
+{
+    public static IServiceCollection AddHealthChecks(this IServiceCollection services, IConfiguration configuration)
+    {
+        var hcBuilder = services.AddHealthChecks();
+
+        hcBuilder
+            .AddUrlGroup(_ => new Uri(configuration["IdentityUrlHC"]), name: "identityapi-check", tags: new string[] { "identityapi" });
+
+        return services;
+    }
+}
